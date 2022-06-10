0614ff88f6b5bcee8cd6577c3ea390b42675b946
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-10 13:45:26 +0300

    Add task solution

--- L02_AsynchronousProgramming/Lab/T03GitHubCommits/app.js
+++ L02_AsynchronousProgramming/Lab/T03GitHubCommits/app.js
@@ -1,4 +1,24 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    const inputUsername = document.getElementById('username');
+    const repoUsername = document.getElementById('repo');
+    const ulList = document.getElementById('commits');
+
+    while (ulList.firstChild) {
+        ulList.removeChild(ulList.firstChild);
+    }
+
+    const url = `https://api.github.com/repos/${inputUsername.value}/${repoUsername.value}/commits`;
+
+    fetch(url)
+        .then(res => res.ok ? res.json() : createElement(`Error: ${res.status}(${res.statusText})`))
+        .then((data) =>
+            data.forEach(({ commit }) => {
+                createElement(`${commit.author.name}: ${commit.message}`);
+            }));
+
+    function createElement(liContent) {
+        const li = document.createElement('li');
+        li.textContent = liContent;
+        ulList.appendChild(li);
+    }
 }
