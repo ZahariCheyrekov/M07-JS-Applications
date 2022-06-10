eeb24e195b95e7e193aefde2bcdae95fa059c493
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-10 13:06:39 +0300

    Add task solution

--- L02_AsynchronousProgramming/Lab/T02GitHubRepos/scripts/app.js
+++ L02_AsynchronousProgramming/Lab/T02GitHubRepos/scripts/app.js
@@ -1,3 +1,23 @@
 function loadRepos() {
-	console.log("TODO...");
+	const username = document.getElementById('username').value;
+	const ulList = document.getElementById('repos');
+
+	while (ulList.firstChild) {
+		ulList.removeChild(ulList.firstChild);
+	}
+
+	const url = `https://api.github.com/users/${username}/repos`;
+
+	fetch(url)
+		.then((response) => response.json())
+		.then(data => data.forEach(({ full_name, html_url }) => {
+			const li = document.createElement('li');
+			const a = document.createElement('a');
+			a.textContent = full_name;
+			a.href = html_url;
+
+			li.appendChild(a);
+			ulList.appendChild(li);
+		}))
+		.catch((error) => console.log(error));
 }
