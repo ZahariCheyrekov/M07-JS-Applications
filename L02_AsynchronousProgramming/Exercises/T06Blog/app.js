b282a43a32c2a54d10b26670083a50f7eba0b59b
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-12 19:14:51 +0300

    Add viewPosts function

--- L02_AsynchronousProgramming/Exercises/T06Blog/app.js
+++ L02_AsynchronousProgramming/Exercises/T06Blog/app.js
@@ -2,6 +2,9 @@ function attachEvents() {
     const loadPostsBtn = document.getElementById('btnLoadPosts');
     const viewPostsBtn = document.getElementById('btnViewPost');
     const posts = document.getElementById('posts');
+    const postTitle = document.getElementById('post-title');
+    const postBody = document.getElementById('post-body');
+    const commentsUl = document.getElementById('post-comments');
     let loaded = false;
 
     const postsUrl = 'http://localhost:3030/jsonstore/blog/posts';
@@ -24,16 +27,36 @@ function attachEvents() {
     });
 
     viewPostsBtn.addEventListener('click', async () => {
+        const selected = Array.from(posts.querySelectorAll('option')).find(o => o.selected);
 
+        const res = await fetch(postsUrl + `/${selected.value}`);
+        const data = await res.json();
+
+        postTitle.textContent = data.title;
+        postBody.textContent = data.body;
+
+        const resComments = await fetch(commentsUrl);
+        const dataComments = await resComments.json();
+
+        Object.keys(dataComments).forEach(key => {
+            console.log(key, selected.value)
+            if (key == selected.value) {
+                const li = document.createElement('li');
+                li.id = commentsData[key].id;
+                li.textContent = commentsData[key].text;
+                commentsUl.appendChild(li);
+                console.log(li)
+            }
+        });
     });
+}
 
-    function createComponent(data, key) {
-        const component = document.createElement('option');
-        component.setAttribute('value', key);
-        component.setAttribute('id', data[key].id);
-        component.textContent = data[key].title.toUpperCase();
-        return component;
-    }
+function createComponent(data, key) {
+    const component = document.createElement('option');
+    component.setAttribute('value', key);
+    component.setAttribute('id', data[key].id);
+    component.textContent = data[key].title.toUpperCase();
+    return component;
 }
 
 attachEvents();
