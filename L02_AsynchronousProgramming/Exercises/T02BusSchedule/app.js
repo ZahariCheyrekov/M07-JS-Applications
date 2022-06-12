12feadf0cb7030a4e74da8835e6e361444909be8
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-12 13:08:41 +0300

    Add solve function logic

--- L02_AsynchronousProgramming/Exercises/T02BusSchedule/app.js
+++ L02_AsynchronousProgramming/Exercises/T02BusSchedule/app.js
@@ -1,17 +1,35 @@
 function solve() {
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
+    const infoSpan = document.querySelector('.info');
+    const url = 'http://localhost:3030/jsonstore/bus/schedule/2572';
 
-    function depart() {
-        console.log('Depart TODO...');
+    async function depart() {
+        disableButtons(true, false);
+
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+
+            const { next } = data;
+            infoSpan.textContent = `Next stop ${next.slice(0, 1).toUpperCase() + next.slice(1)}`;
+
+        } catch (error) {
+            infoSpan.textContent = 'Error';
+            disableButtons(true, true);
+        }
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        disableButtons(false, true);
     }
 
     return {
         depart,
         arrive
     };
+
+   
 }
 
 let result = solve();
