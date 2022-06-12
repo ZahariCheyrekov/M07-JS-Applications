ee2ef3599fe8831e26870396bcb87d8a382c5a14
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-12 18:01:00 +0300

    Add attachEvents function

--- L02_AsynchronousProgramming/Exercises/T06Blog/app.js
+++ L02_AsynchronousProgramming/Exercises/T06Blog/app.js
@@ -1,5 +1,17 @@
 function attachEvents() {
-    console.log('TODO...');
+    const loadPostsBtn = document.getElementById('btnLoadPosts');
+    const viewPostsBtn = document.getElementById('btnViewPost');
+
+    const postsUrl = 'http://localhost:3030/jsonstore/blog/posts';
+    const commentsUrl = 'http://localhost:3030/jsonstore/blog/comments';
+
+    loadPostsBtn.addEventListener('click', async () => {
+
+    });
+
+    viewPostsBtn.addEventListener('click', async () => {
+        
+    });
 }
 
 attachEvents();
