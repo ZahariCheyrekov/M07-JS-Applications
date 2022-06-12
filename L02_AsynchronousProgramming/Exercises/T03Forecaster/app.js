9f9eb8ec38a9c35023f9d0537e4168ba58973213
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-12 14:17:41 +0300

    Add create functions

--- L02_AsynchronousProgramming/Exercises/T03Forecaster/app.js
+++ L02_AsynchronousProgramming/Exercises/T03Forecaster/app.js
@@ -14,7 +14,7 @@ async function attachEvents() {
             const response = await fetch(locationUrl);
             const data = await response.json();
 
-
+            createCurrent(data);
 
             const upcomingUrl = `http://localhost:3030/jsonstore/forecaster/upcoming/${idField.value}`;
             const res = await fetch(locationUrl);
@@ -22,30 +22,43 @@ async function attachEvents() {
 
             forecast.style.display = '';
 
+            createUpcoming(dataUpcoming);
+
         } catch (error) {
             label.textContent = 'Error';
             forecast.style.display = '';
             upcoming.style.display = 'none';
         }
-    };
-}
+    }
 
-function createComponent(type, content) {
-    const element = document.createElement(type);
-    element.textContent = content;
-    return element;
-}
+    function createCurrent(data) {
+        const divForecasts = createComponent('div', undefined, 'forecasts');
+        const conditionSymbol = createComponent('span', getWeatherIcon(data.forecast.condition), 'condition symbol')
+        console.log(data);
+    }
 
-function getWeatherIcon(condition) {
-    const types = {
-        'Sunny': '&#x2600',
-        'Partly sunny': '&#x26C5',
-        'Overcast': '&#x2601',
-        'Rain': '&#x2614',
-        'Degrees': '&#176'
-    };
+    function createUpcoming(dataUpcoming) {
 
-    return types[condition];
+    }
+
+    function createComponent(type, content, className) {
+        const element = document.createElement(type);
+        element.textContent = content;
+        element.classList.add(className);
+        return element;
+    }
+
+    function getWeatherIcon(condition) {
+        const types = {
+            'Sunny': '&#x2600',
+            'Partly sunny': '&#x26C5',
+            'Overcast': '&#x2601',
+            'Rain': '&#x2614',
+            'Degrees': '&#176'
+        };
+
+        return types[condition];
+    }
 }
 
 attachEvents();
