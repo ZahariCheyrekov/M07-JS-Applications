fb3c8b51fe06c67553788d8fcd88aff739fbf8f3
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-12 13:59:16 +0300

    Create getData function

--- L02_AsynchronousProgramming/Exercises/T03Forecaster/app.js
+++ L02_AsynchronousProgramming/Exercises/T03Forecaster/app.js
@@ -8,7 +8,24 @@ async function attachEvents() {
 
     submitBtn.addEventListener('click', getData);
 
-    
+    async function getData() {
+        try {
+            const locationUrl = `http://localhost:3030/jsonstore/forecaster/today/${idField.value}`;
+            const response = await fetch(locationUrl);
+            const data = await response.json();
+
+            const upcomingUrl = `http://localhost:3030/jsonstore/forecaster/upcoming/${idField.value}`;
+            const res = await fetch(locationUrl);
+            const dataUpcoming = await res.json();
+
+
+
+        } catch (error) {
+            label.textContent = 'Error';
+            forecast.style.display = '';
+            upcoming.style.display = 'none';
+        }
+    };
 }
 
 attachEvents();
