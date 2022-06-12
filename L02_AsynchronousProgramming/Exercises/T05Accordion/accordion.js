b1b772df796e6165b50eea1e31b204889065ce6a
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-12 17:47:51 +0300

    Add createArticle function

--- L02_AsynchronousProgramming/Exercises/T05Accordion/accordion.js
+++ L02_AsynchronousProgramming/Exercises/T05Accordion/accordion.js
@@ -10,5 +10,36 @@ async function solution() {
     });
 }
 
+function createArticle({ _id, title }) {
+    const accordion = createComponent('div', 'accordion', '');
+    const head = createComponent('div', 'head', '');
+    const titleSpan = createComponent('span', '', title);
+    const moreBtn = createComponent('button', 'button', 'More');
+    const extraDiv = createComponent('div', 'extra', '');
+    extraDiv.style.display = 'none';
+    const contentParagraph = createComponent('p', '', '');
+    moreBtn.id = _id;
+
+    head.append(titleSpan, moreBtn);
+    extraDiv.appendChild(contentParagraph);
+    accordion.append(head, extraDiv);
+
+    moreBtn.addEventListener('click', async () => {
+        if (extraDiv.style.display == 'none') {
+            const response = await fetch(`http://localhost:3030/jsonstore/advanced/articles/details/${_id}`);
+            const dataEl = await response.json();
+
+            moreBtn.textContent = 'Less';
+            extraDiv.style.display = '';
+            contentParagraph.textContent = dataEl.content;
+        } else {
+            moreBtn.textContent = 'More';
+            extraDiv.style.display = 'none';
+        }
+    });
+
+    return accordion;
+}
+
 
 solution();
