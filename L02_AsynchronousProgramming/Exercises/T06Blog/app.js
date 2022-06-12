b6cc601ef75d2e140c296f484ab7873153b6fb8f
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-12 18:15:22 +0300

    Add loadPostsBtn eventListener

--- L02_AsynchronousProgramming/Exercises/T06Blog/app.js
+++ L02_AsynchronousProgramming/Exercises/T06Blog/app.js
@@ -1,17 +1,33 @@
-function attachEvents() {
+async function attachEvents() {
     const loadPostsBtn = document.getElementById('btnLoadPosts');
     const viewPostsBtn = document.getElementById('btnViewPost');
+    const posts = document.getElementById('posts');
+    let loaded = false;
 
     const postsUrl = 'http://localhost:3030/jsonstore/blog/posts';
     const commentsUrl = 'http://localhost:3030/jsonstore/blog/comments';
 
     loadPostsBtn.addEventListener('click', async () => {
+        if (loaded) {
+            return;
+        }
 
+        fetch(postsUrl)
+            .then(res => res.json())
+            .then(data => {
+                Object.keys(data).forEach(key => {
+                    const option = createComponent(data, key);
+                    posts.appendChild(option);
+                });
+            });
+        loaded = true;
     });
 
     viewPostsBtn.addEventListener('click', async () => {
-        
+
     });
+
+    
 }
 
 attachEvents();
