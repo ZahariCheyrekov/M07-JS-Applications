e349163c0f008e98dca26f6fcf827b37de02fd56
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-12 17:47:33 +0300

    Add solve function

--- L02_AsynchronousProgramming/Exercises/T05Accordion/accordion.js
+++ L02_AsynchronousProgramming/Exercises/T05Accordion/accordion.js
@@ -1,3 +1,14 @@
-function solution() {
-    //TODO .....
+async function solution() {
+    const main = document.getElementById('main');
+    const url = 'http://localhost:3030/jsonstore/advanced/articles/list';
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    data.forEach(dataEl => {
+        main.appendChild(createArticle(dataEl));
+    });
 }
+
+
+solution();
