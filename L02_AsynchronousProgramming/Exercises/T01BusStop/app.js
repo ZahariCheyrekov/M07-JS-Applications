1e220a80f737fd1508a71525bab383c17468d990
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-12 12:15:56 +0300

    Add fetch logic

--- L02_AsynchronousProgramming/Exercises/T01BusStop/app.js
+++ L02_AsynchronousProgramming/Exercises/T01BusStop/app.js
@@ -1,3 +1,22 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    const idField = document.getElementById('stopId');
+    const divResult = document.getElementById('stopName');
+
+    const url = `http://localhost:3030/jsonstore/bus/businfo/${idField.value}`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        const { name } = data;
+        divResult.textContent = name;
+
+        // Bus {busId} arrives in {time} minutes
+
+    } catch (error) {
+        divResult.textContent = 'Error';
+    }
+
+    idField.value = '';
+
 }
