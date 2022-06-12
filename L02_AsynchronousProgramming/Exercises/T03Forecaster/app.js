a9a49122602e767cdaa8909534a4f894ee4428e0
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-12 13:59:04 +0300

    Get elements

--- L02_AsynchronousProgramming/Exercises/T03Forecaster/app.js
+++ L02_AsynchronousProgramming/Exercises/T03Forecaster/app.js
@@ -1,5 +1,14 @@
-function attachEvents() {
-    console.log("TODO...");
+async function attachEvents() {
+    const idField = document.getElementById('location');
+    const submitBtn = document.getElementById('submit');
+    const forecast = document.getElementById('forecast');
+    const upcoming = document.getElementById('upcoming');
+    const label = document.querySelector('.label');
+    const url = 'http://localhost:3030/jsonstore/forecaster/locations';
+
+    submitBtn.addEventListener('click', getData);
+
+    
 }
 
 attachEvents();
