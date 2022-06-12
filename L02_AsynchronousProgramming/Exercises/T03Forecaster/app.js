b285bfb0e952b712fc1cb40ad95bc9f3e2b817b5
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-12 15:05:58 +0300

    Add task solution

--- L02_AsynchronousProgramming/Exercises/T03Forecaster/app.js
+++ L02_AsynchronousProgramming/Exercises/T03Forecaster/app.js
@@ -5,7 +5,6 @@ async function attachEvents() {
     const current = document.getElementById('current');
     const upcoming = document.getElementById('upcoming');
     const label = document.querySelector('.label');
-    const url = 'http://localhost:3030/jsonstore/forecaster/locations';
 
     submitBtn.addEventListener('click', getData);
 
