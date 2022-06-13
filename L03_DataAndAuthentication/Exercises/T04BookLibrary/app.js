17f29672df493f2ea7ffb8a6e5b9b7616b430f1b
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-13 19:04:20 +0300

    Add loadBooksBtn logic

--- L03_DataAndAuthentication/Exercises/T04BookLibrary/app.js
+++ L03_DataAndAuthentication/Exercises/T04BookLibrary/app.js
@@ -1,1 +1,17 @@
-console.log('My requests...')
+const loadBooksBtn = document.getElementById('loadBooks');
+const tbody = document.querySelector('tbody');
+const url = 'http://localhost:3030/jsonstore/collections/books';
+
+loadBooksBtn.addEventListener('click', () => {
+    tbody.replaceChildren();
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            Object.values(data).forEach(book => {
+
+            });
+        })
+        .catch(error => console.log(error));
+});
+
