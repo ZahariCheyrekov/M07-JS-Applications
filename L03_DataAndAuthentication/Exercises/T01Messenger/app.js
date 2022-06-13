80d4f5cbc78977127f91a00d50556cf0fa15b816
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-13 13:37:40 +0300

    Get elements

--- L03_DataAndAuthentication/Exercises/T01Messenger/app.js
+++ L03_DataAndAuthentication/Exercises/T01Messenger/app.js
@@ -1,5 +1,18 @@
+const url = 'http://localhost:3030/jsonstore/messenger';
+
 function attachEvents() {
-    console.log('TODO...');
+    const submitBtn = document.getElementById('submit');
+    const refreshBtn = document.getElementById('refresh');
+    refreshBtn.addEventListener('click', getAllMessages);
+}
+
+function getAllMessages() {
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => console.log(error));
 }
 
 attachEvents();
