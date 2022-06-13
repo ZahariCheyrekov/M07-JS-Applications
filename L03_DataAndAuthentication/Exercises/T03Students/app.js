bc2a54c974bf281f066104fd10a0ce670cda19d7
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-13 15:56:03 +0300

    Get html data

--- L03_DataAndAuthentication/Exercises/T03Students/app.js
+++ L03_DataAndAuthentication/Exercises/T03Students/app.js
@@ -1,1 +1,12 @@
-console.log('TODO...');
+const url = 'http://localhost:3030/jsonstore/collections/students';
+const submitBtn = document.getElementById('submit');
+
+function solve() {
+    submitBtn.addEventListener('click', loadStudents)
+}
+
+function loadStudents(ev) {
+    ev.preventDefault();
+}
+
+solve();
