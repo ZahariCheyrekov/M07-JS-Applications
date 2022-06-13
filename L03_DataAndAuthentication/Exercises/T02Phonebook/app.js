b34d20c20d308ce1fd293d05863c05e161d3b6b6
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-13 14:21:05 +0300

    Add getAllNumbers function

--- L03_DataAndAuthentication/Exercises/T02Phonebook/app.js
+++ L03_DataAndAuthentication/Exercises/T02Phonebook/app.js
@@ -1,9 +1,35 @@
 const loadBtn = document.getElementById('btnLoad');
+const phonebook = document.getElementById('phonebook');
 
 const url = 'http://localhost:3030/jsonstore/phonebook';
 
 function attachEvents() {
-    loadBtn.addEventListener
+    loadBtn.addEventListener('click', getAllNumbers);
 }
+
+function getAllNumbers() {
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            for (const { person, phone, _id } of Object.values(data)) {
+                const li = document.createElement('li');
+                li.textContent = `${person}: ${phone}`;
+                li.id = _id;
+
+                const deleteBtn = document.createElement('button');
+                deleteBtn.textContent = 'Delete';
+                deleteBtn.addEventListener('click', () => {
+                    fetch(`${url}/${_id}`, {
+                        method: 'DELETE'
+                    });
+                });
+
+                li.appendChild(deleteBtn);
+                phonebook.appendChild(li);
+            }
+        })
+        .catch(error => console.log(error));
+}
+
 
 attachEvents();
