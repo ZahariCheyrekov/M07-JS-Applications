2794945bd06d87f9cc6303d60fc8de816efcb93e
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-13 13:51:30 +0300

    Add sendMessage function logic

--- L03_DataAndAuthentication/Exercises/T01Messenger/app.js
+++ L03_DataAndAuthentication/Exercises/T01Messenger/app.js
@@ -1,10 +1,28 @@
-const url = 'http://localhost:3030/jsonstore/messenger';
 const textArea = document.getElementById('messages');
+const nameInput = document.getElementsByName('author')[0];
+const contentInput = document.getElementsByName('content')[0];
+const url = 'http://localhost:3030/jsonstore/messenger';
 
 function attachEvents() {
     const submitBtn = document.getElementById('submit');
+    submitBtn.addEventListener('click', sendMessage);
     const refreshBtn = document.getElementById('refresh');
     refreshBtn.addEventListener('click', getAllMessages);
+}
+
+function sendMessage() {
+    if (!nameInput.value || !contentInput.value) {
+        return;
+    }
+
+    fetch(url, {
+        method: 'POST',
+        'Content-Type': 'application/json',
+        body: JSON.stringify({
+            author: nameInput.value,
+            message: contentInput.value
+        })
+    })
 }
 
 function getAllMessages() {
