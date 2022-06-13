18936855cb25e636719ba3645ffe4fb4a1f8b37c
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-13 16:01:57 +0300

    Add createTableRow function

--- L03_DataAndAuthentication/Exercises/T03Students/app.js
+++ L03_DataAndAuthentication/Exercises/T03Students/app.js
@@ -2,10 +2,27 @@ const url = 'http://localhost:3030/jsonstore/collections/students';
 const submitBtn = document.getElementById('submit');
 
 function solve() {
-    submitBtn.addEventListener('click', loadStudents)
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            Object.values(data).forEach(student => {
+
+                createTableRow(student);
+            })
+        })
+        .catch(error => console.log(error));
+
+    submitBtn.addEventListener('click', createStudent);
 }
 
-function loadStudents(ev) {
+function createTableRow(student) {
+    let { firstName, lastName, facultyNumber, grade } = student;
+    grade = Number(grade);
+
+    const tr = document.createElement('tr');
+}
+
+function createStudent(ev) {
     ev.preventDefault();
 }
 
