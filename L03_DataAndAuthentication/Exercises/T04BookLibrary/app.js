5b7511f7e50888e39f74c748a70fbedd070274ad
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-13 19:10:27 +0300

    Add createTableData function

--- L03_DataAndAuthentication/Exercises/T04BookLibrary/app.js
+++ L03_DataAndAuthentication/Exercises/T04BookLibrary/app.js
@@ -1,17 +1,35 @@
-const loadBooksBtn = document.getElementById('loadBooks');
-const tbody = document.querySelector('tbody');
-const url = 'http://localhost:3030/jsonstore/collections/books';
+function solve() {
 
-loadBooksBtn.addEventListener('click', () => {
-    tbody.replaceChildren();
+    const loadBooksBtn = document.getElementById('loadBooks');
+    const tbody = document.querySelector('tbody');
+    const url = 'http://localhost:3030/jsonstore/collections/books';
 
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            Object.values(data).forEach(book => {
+    loadBooksBtn.addEventListener('click', () => {
+        tbody.replaceChildren();
 
-            });
-        })
-        .catch(error => console.log(error));
-});
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                Object.values(data).forEach(book => {
+                    console.log(book)
+                    // tbody.appendChild(createBook(book));
+                });
+            })
+            .catch(error => console.log(error));
+    });
 
+    function createBook({ author, title }) {
+        const tr = document.createElement('tr');
+
+        const authorName = createTableData(tr, author);
+        const titleName = createTableData(tr, title);
+    }
+
+    function createTableData(value) {
+        const td = document.createElement('td');
+        td.textContent = value;
+        return td;
+    }
+}
+
+solve();
