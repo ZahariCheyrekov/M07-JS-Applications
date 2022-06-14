dc2cd6343b560cf4859a836cfb322c548e9276cc
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-14 10:28:08 +0300

    Add login js logic

--- L03_DataAndAuthentication/Exercises/T05FisherGame/src/login.js
+++ L03_DataAndAuthentication/Exercises/T05FisherGame/src/login.js
@@ -1,1 +1,39 @@
-console.log('TODO:// Implement Login functionality');
+const form = document.getElementById('loginForm');
+const url = 'http://localhost:3030/users/login';
+
+form.addEventListener('submit', login);
+
+async function login(ev) {
+    ev.preventDefault();
+
+    const formData = new FormData(e.target);
+
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    if (!email.trim() || !password.trim()) {
+        alert('Fill all fields!');
+        return;
+    }
+
+    const res = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email,
+            password
+        })
+    });
+
+    if (!res.ok) {
+        const error = await res.json();
+        alert(`Error: ${error.message}`);
+        return;
+    }
+
+    const data = await res.json();
+    sessionStorage.setItem('userData', JSON.stringify({ token: data.accessToken, _id: data._id }))
+    location.pathname = 'index.html';
+}
