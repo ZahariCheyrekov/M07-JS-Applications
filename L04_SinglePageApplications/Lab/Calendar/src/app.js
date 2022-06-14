d4a2bb9cc61d943a3981d5723f7d2b68bbe8fd1a
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-14 14:35:03 +0300

    Add task solution

--- L04_SinglePageApplications/Lab/Calendar/src/app.js
+++ L04_SinglePageApplications/Lab/Calendar/src/app.js
@@ -1,30 +1,45 @@
 import { rednderElement } from './renderer.js';
-import { validateYear } from './validator.js';
+import { validateMonth, validateYear } from './validator.js';
 
 const yearsBase = document.getElementById('years');
 const years = Array.from(document.querySelectorAll('.monthCalendar')).reduce((year, data) => {
     year[data.id] = data;
     return year;
 }, {});
-const months = Array.from(document.querySelectorAll('.daysCalendar'))
-console.log()
+const monthsElements = Array.from(document.querySelectorAll('.daysCalendar'));
 
 rednderElement(yearsBase);
 
 document.addEventListener('click', (ev) => {
-    console.log(ev.target);
-
     if (validateYear(ev)) {
-        console.log(ev.target.textContent)
         const year = ev.target.textContent.trim();
         rednderElement(years[`year-${year}`]);
+
+    } else if (validateMonth(ev)) {
+        const year = document.querySelector('caption').textContent;
+        const month = ev.target.innerText.trim();
+        const monthIndex = months.findIndex(x => x == month);
+
+        if (monthIndex !== -1) {
+            const monthElement = monthsElements.find(x => x.id === `month-${year}-${monthIndex + 1}`);
+            rednderElement(monthElement);
+        }
     }
 
     if (ev.target.tagName == 'CAPTION') {
-        const captionText = ev.target.innerText
+        const dataText = ev.target.innerText;
 
-        if (!isNaN(captionText)) {
+        if (dataText.match(/^([1-2]{1}[0-9]{3}) \- ([1-2]{1}[0-9]{3})$/g)) {
+            return;
+        }
+
+        if (isNaN(dataText)) {
+            const year = dataText.match(/[1-2]{1}[0-9]{3}/g);
+            rednderElement(years[`year-${year}`]);
+        } else {
             rednderElement(yearsBase);
         }
     }
 });
+
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
--- L04_SinglePageApplications/Lab/Calendar/src/validator.js
+++ L04_SinglePageApplications/Lab/Calendar/src/validator.js
@@ -1,3 +1,8 @@
 export function validateYear(ev) {
-    return ev.target.className == 'day' || ev.target.className == 'date';
+    return ev.path.some(y => y.id === 'years' && y.tagName == 'SECTION') &&
+        (ev.target.className == 'date' || ev.target.className == 'day');
+}
+
+export function validateMonth(ev) {
+    return ev.path.some(m => m.className == 'monthCalendar' && m.tagName == 'SECTION');
 }
