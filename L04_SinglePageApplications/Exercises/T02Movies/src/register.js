4481586b00ca6f0f11f739128288c7b928339187
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-15 15:07:28 +0300

    Add getData function

--- L04_SinglePageApplications/Exercises/T02Movies/src/register.js
+++ L04_SinglePageApplications/Exercises/T02Movies/src/register.js
@@ -1,7 +1,23 @@
 import { loadSection } from './utils.js';
 
 const registerSection = document.querySelector('#form-sign-up');
+const form = registerSection.querySelector('.text-center.border.border-light.p-5');
+form.addEventListener('submit', getData);
 
 export function registerPage() {
     loadSection(registerSection);
+}
+
+function getData(ev) {
+    ev.preventDefault();
+
+    const formData = new FormData(form);
+    const email = formData.get('email');
+    const password = formData.get('password');
+    const repeatPassword = formData.get('repeatPassword');
+
+    validateInput(email, password, repeatPassword);
+    registerUser(email, password, repeatPassword);
+
+
 }
