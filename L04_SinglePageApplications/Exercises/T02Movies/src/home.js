bdd9b30ba3c1e4abec21800b17096aae5b2938d5
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-15 14:12:11 +0300

    Add showMovies function

--- L04_SinglePageApplications/Exercises/T02Movies/src/home.js
+++ L04_SinglePageApplications/Exercises/T02Movies/src/home.js
@@ -1,6 +1,8 @@
 import { loadSection } from './utils.js';
+import { loading } from './utils.js';
 
 const homeSection = document.getElementById('home-page');
+const movieCatalog = homeSection.querySelector('.card-deck.d-flex.justify-content-center');
 
 export function homePage() {
     loadSection(homeSection);
@@ -8,11 +10,32 @@ export function homePage() {
 }
 
 async function showMovies() {
-    
+    movieCatalog.replaceChildren(loading());
     const movies = await getMovies();
+
+    movieCatalog.replaceChildren(...movies.map(createMovieTemplate));
+}
+
+function createMovieTemplate(movie) {
+    const element = document.createElement('div');
+    element.className = 'card mb-4';
+    element.innerHTML = `
+        <img class="card-img-top" src="${movie.img}"
+            alt="Card image cap" width="400">
+        <div class="card-body">
+            <h4 class="card-title">${movie.title}</h4>
+        </div>
+        <div class="card-footer">
+            <a href="/details/${movie._id}">
+                <button data-id="${movie._id}" type="button" class="btn btn-info">Details</button>
+            </a>
+        </div>`;
+
+    return element;
 }
 
 async function getMovies() {
+    homeSection
     const res = await fetch('http://localhost:3030/data/movies');
     const movies = await res.json();
 
