22f14d65fed0817f9113378988b2db354ae67361
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-15 13:57:52 +0300

    Add navigation logic

--- L04_SinglePageApplications/Exercises/T02Movies/src/app.js
+++ L04_SinglePageApplications/Exercises/T02Movies/src/app.js
@@ -4,11 +4,24 @@ import { loginPage } from './login.js';
 const routes = {
     '/': homePage,
     '/login': loginPage,
+    '/register': registerPage
 };
 
-const navigation = document.querySelector('navbar');
+const navigation = document.querySelector('.navbar');
 
 navigation.addEventListener('click', onNavigate);
 
+function onNavigate(ev) {
+    if (ev.target.tagName == 'A' && ev.target.href) {
+        ev.preventDefault();
+
+        const url = new URL(ev.target.href);
+        const view = routes[url.pathname];
+
+        if (typeof view == 'function') {
+            view();
+        }
+    }
+}
 
 homePage();
