7f1a9d82de764cbae0c818f44eae457d98dfde42
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-15 14:29:33 +0300

    Add loginUser function

--- L04_SinglePageApplications/Exercises/T02Movies/src/login.js
+++ L04_SinglePageApplications/Exercises/T02Movies/src/login.js
@@ -17,3 +17,25 @@ function getData(ev) {
 
     loginUser(email, password);
 }
+
+async function loginUser(email, password) {
+    try {
+        const res = await fetch('http://localhost:3030/users/login', {
+            method: 'POST',
+            'Content-Type': 'application/json',
+            body: JSON.stringify({ email, password })
+        });
+
+        if (!res.ok) {
+            const error = await res.json();
+            throw new Error(error.message);
+        }
+
+        const user = await res.json();
+        localStorage.setItem('user', JSON.stringify(user));
+
+    } catch (error) {
+        alert(error.message);
+        throw error;
+    }
+}
