639e61d08879ea76a0634ee5f0b69327be958b1f
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-15 15:41:25 +0300

    Add getData function

--- L04_SinglePageApplications/Exercises/T02Movies/src/create.js
+++ L04_SinglePageApplications/Exercises/T02Movies/src/create.js
@@ -1,7 +1,22 @@
 import { loadSection } from './utils.js';
 
 const addMovieSection = document.querySelector('#add-movie');
+const form = document.querySelector('.text-center.border.border-light.p-5');
+form.addEventListener('submit', getData);
 
 export function createPage() {
     loadSection(addMovieSection);
 }
+
+function getData(ev) {
+    ev.preventDefault();
+
+    const formData = new FormData(form);
+    const title = formData.get('title');
+    const description = formData.get('description');
+    const img = formData.get('imageUrl');
+
+    validateInput(title, description, img);
+    createMovie(title, description, img);
+}
+
