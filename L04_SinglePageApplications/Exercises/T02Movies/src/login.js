74b7ae543543423801c4b11f3aadb73a471fa04d
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-15 14:29:22 +0300

    Add getData function

--- L04_SinglePageApplications/Exercises/T02Movies/src/login.js
+++ L04_SinglePageApplications/Exercises/T02Movies/src/login.js
@@ -1,7 +1,19 @@
 import { loadSection } from './utils.js';
 
 const loginSection = document.getElementById('form-login');
+const form = loginSection.querySelector('.text-center.border.border-light.p-5');
+form.addEventListener('submit', getData);
 
 export function loginPage() {
     loadSection(loginSection);
+}
+
+function getData(ev) {
+    ev.preventDefault();
+
+    const formData = new FormData(form);
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    loginUser(email, password);
 }
