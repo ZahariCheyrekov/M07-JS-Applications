0730d495aa67cb9a1ce16cde65e2508df3adb4c8
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-15 13:53:17 +0300

    Add login js

--- L04_SinglePageApplications/Exercises/T02Movies/src/app.js
+++ L04_SinglePageApplications/Exercises/T02Movies/src/app.js
@@ -1,7 +1,14 @@
 import { homePage } from './home.js';
+import { loginPage } from './login.js';
 
 const routes = {
     '/': homePage,
+    '/login': loginPage,
 };
+
+const navigation = document.querySelector('navbar');
+
+navigation.addEventListener('click', onNavigate);
+
 
 homePage();
---
+++ L04_SinglePageApplications/Exercises/T02Movies/src/login.js
@@ -0,0 +1,7 @@
+import { loadSection } from './utils.js';
+
+const loginSection = document.getElementById('form-login');
+
+export function loginPage() {
+    loadSection(loginSection);
+}
