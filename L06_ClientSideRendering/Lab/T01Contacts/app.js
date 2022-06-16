ea4d76bef38da25d9ffe29daf7bb615bda013b29
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-16 20:01:03 +0300

    Add task solution

---
+++ L06_ClientSideRendering/Lab/T01Contacts/app.js
@@ -0,0 +1,25 @@
+import { render } from './node_modules/lit-html/lit-html.js';
+import { contacts } from '../../../contacts.js';
+import { template } from '../../../template.js';
+
+const root = document.getElementById('contacts');
+const result = contacts.map(template);
+root.addEventListener('click', showDetails);
+
+function showDetails(ev) {
+    const isContaining = ev.target.classList.contains('detailsBtn');
+
+    if (isContaining) {
+        const element = ev.target.parentNode;
+
+        const styleEl = element.querySelector('.details').style.display;
+
+        if (styleEl == 'block') {
+            element.querySelector('.details').style.display = 'none';
+        } else {
+            element.querySelector('.details').style.display = 'block';
+        }
+    }
+}
+
+render(result, root);
--- app.js
+++
@@ -1,3 +0,0 @@
-import { render } from './node_modules/lit-html/lit-html.js';
-import { contacts } from './contacts.js';
-
