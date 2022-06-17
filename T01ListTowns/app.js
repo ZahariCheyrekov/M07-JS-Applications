6a264343a1e08da6ce54783d610efbf84f07deaf
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-17 10:30:02 +0300

    Get DOM elements

---
+++ T01ListTowns/app.js
@@ -0,0 +1,11 @@
+import { render } from '../node_modules/lit-html/lit-html.js';
+
+const root = document.getElementById('root');
+const townInput = document.getElementById('towns');
+const loadBtn = document.getElementById('btnLoadTowns');
+
+loadBtn.addEventListener('click', (ev) => {
+    ev.preventDefault();
+
+    
+});
