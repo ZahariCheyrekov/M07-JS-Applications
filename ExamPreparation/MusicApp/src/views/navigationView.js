3e02290602e799127fe73d93aa2cffd25e3281bd
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-19 12:51:05 +0300

    Add renderNavigation

---
+++ ExamPreparation/MusicApp/src/views/navigationView.js
@@ -0,0 +1,24 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+
+
+const navigationTemplate = () => html`
+    <nav class="header-navigation">
+        <img src="./images/headphones.png">
+        <a href="#">Home</a>
+        <ul>
+            <!--All user-->
+            <li><a href="#">Catalog</a></li>
+            <li><a href="#">Search</a></li>
+            <!--Only guest-->
+            <li><a href="#">Login</a></li>
+            <li><a href="#">Register</a></li>
+            <!--Only user-->
+            <li><a href="#">Create Album</a></li>
+            <li><a href="#">Logout</a></li>
+        </ul>
+    </nav>
+`;
+
+export const navigationView = (ctx) => {
+    return navigationTemplate();
+}
