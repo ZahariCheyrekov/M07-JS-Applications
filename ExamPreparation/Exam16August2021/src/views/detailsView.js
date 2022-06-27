761147f463249293d5cf43bf135f45c584f87ef2
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-27 18:52:11 +0300

    Add comments logic

--- ExamPreparation/Exam16August2021/src/views/detailsView.js
+++ ExamPreparation/Exam16August2021/src/views/detailsView.js
@@ -1,7 +1,9 @@
 import { html, nothing } from '../../node_modules/lit-html/lit-html.js'
 import * as gameService from '../services/gameService.js';
+import * as commentsService from '../services/commentsService.js';
+import { commentsView } from './commentsView.js';
 
-const detailsTemplate = (game, onDelete) => html`
+const detailsTemplate = (game, commentsSection, onDelete) => html`
     <section id="game-details">
         <h1>Game Details</h1>
         <div class="info-section">
@@ -15,60 +17,46 @@ const detailsTemplate = (game, onDelete) => html`
     
             <p class="text">${game.summary}</p>
     
-            <!-- Bonus ( for Guests and Users ) -->
-            <!-- <div class="details-comments">
-                                    <h2>Comments:</h2>
-                                    <ul>
-                                        list all comments for current game (If any)
-                                        <li class="comment">
-                                            <p>Content: I rate this one quite highly.</p>
-                                        </li>
-                                        <li class="comment">
-                                            <p>Content: The best game.</p>
-                                        </li>
-                                    </ul>
-                                    Display paragraph: If there are no games in the database -->
-            <!-- <p class="no-comment">No comments.</p>
-                                </div> -->
+            ${commentsSection}
     
-            <!-- Edit/Delete buttons ( Only for creator of this game )  -->
-           
-            ${game.isOwner  ? html`
+            ${game.isOwner ? html`
             <div class="buttons">
                 <a href="/edit/${game._id}" class="button">Edit</a>
                 <a @click=${onDelete} href="javascript:void(0)" class="button">Delete</a>
-    	    </div>` : nothing}
-            
+            </div>` : nothing}
+    
         </div>
     
         <!-- Bonus -->
         <!-- Add Comment ( Only for logged-in users, which is not creators of the current game ) -->
         <!-- <article class="create-comment">
-                                <label>Add new comment:</label>
-                                <form class="form">
-                                    <textarea name="comment" placeholder="Comment......"></textarea>
-                                    <input class="btn submit" type="submit" value="Add Comment">
-                                </form>
-                            </article> -->
+                              <label>Add new comment:</label>
+                              <form class="form">
+                                  <textarea name="comment" placeholder="Comment......"></textarea>
+                                  <input class="btn submit" type="submit" value="Add Comment">
+                              </form>
+                          </article> -->
     
     </section>
 `;
 
 export async function detailsPage(ctx) {
     const gameId = ctx.params.id;
-    const game = await gameService.getById(gameId);
-
+    const [game, commentsSection] = await Promise.all([
+        gameService.getById(gameId),
+        commentsView(gameId)
+    ]);
 
     if (ctx.user) {
         game.isOwner = ctx.user._id == game._ownerId;
     }
 
-    ctx.render(detailsTemplate(game, onDelete));
+    ctx.render(detailsTemplate(game, commentsSection, onDelete));
 
     async function onDelete() {
         const result = confirm('Are you sure you want to delete this game?');
-    
-        if(result) {
+
+        if (result) {
             await gameService.deleteById(gameId);
             ctx.page.redirect('/');
         }
