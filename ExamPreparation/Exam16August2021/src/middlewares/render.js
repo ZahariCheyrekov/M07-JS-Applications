92af5cb1b1bfcbcdc5c363777b113e4453c89fce
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-27 17:16:19 +0300

    Add types of navigation

--- ExamPreparation/Exam16August2021/src/middlewares/render.js
+++ ExamPreparation/Exam16August2021/src/middlewares/render.js
@@ -2,20 +2,25 @@ import { html, render } from '../../node_modules/lit-html/lit-html.js'
 
 const root = document.getElementById('main-content');
 
-const navTemplate = () => html`
+const userNav = () => html`
+    <div id="user">
+        <a href="/create">Create Game</a>
+        <a href="/logout">Logout</a>
+    </div>
+`;
+
+const noUserNav = () => html`
+    <div id="guest">
+        <a href="/login">Login</a>
+        <a href="/register">Register</a>
+    </div>
+`;
+
+const navTemplate = (user) => html`
     <h1><a class="home" href="/">GamesPlay</a></h1>
     <nav>
         <a href="/catalog">All games</a>
-        <!-- Logged-in users -->
-        <div id="user">
-            <a href="/create">Create Game</a>
-            <a href="/logout">Logout</a>
-        </div>
-        <!-- Guest users -->
-        <div id="guest">
-            <a href="/login">Login</a>
-            <a href="/register">Register</a>
-        </div>
+        ${user ? userNav : noUserNav}
     </nav>
 `;
 
