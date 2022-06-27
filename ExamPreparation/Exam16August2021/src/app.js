57c75f4ad2e9e1e1c88efc47cca312d5bd325621
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-27 14:39:37 +0300

    Add createView

--- ExamPreparation/Exam16August2021/src/app.js
+++ ExamPreparation/Exam16August2021/src/app.js
@@ -1,6 +1,7 @@
 import page from '../node_modules/page/page.mjs';
 import { addRender } from './middlewares/render.js';
 import { catalogPage } from './views/catalogView.js';
+import { createPage } from './views/createView.js';
 import { homePage } from './views/homeView.js';
 import { loginPage } from './views/loginView.js';
 import { registerPage } from './views/registerView.js';
@@ -11,5 +12,6 @@ page('/', homePage);
 page('/catalog', catalogPage);
 page('/login', loginPage);
 page('/register', registerPage);
+page('/create', createPage);
 
 page.start();
