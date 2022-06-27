c4eb9a7cee29dd186fc2a2b9c584e93f43ad2b71
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-27 14:42:06 +0300

    Add detailsView

--- ExamPreparation/Exam16August2021/src/app.js
+++ ExamPreparation/Exam16August2021/src/app.js
@@ -2,6 +2,8 @@ import page from '../node_modules/page/page.mjs';
 import { addRender } from './middlewares/render.js';
 import { catalogPage } from './views/catalogView.js';
 import { createPage } from './views/createView.js';
+import { detailsPage } from './views/detailsView.js';
+import { editPage } from './views/editView.js';
 import { homePage } from './views/homeView.js';
 import { loginPage } from './views/loginView.js';
 import { registerPage } from './views/registerView.js';
@@ -13,5 +15,7 @@ page('/catalog', catalogPage);
 page('/login', loginPage);
 page('/register', registerPage);
 page('/create', createPage);
+page('/details/:id', detailsPage);
+page('/edit/:id', editPage);
 
 page.start();
