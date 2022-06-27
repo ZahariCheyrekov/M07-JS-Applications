15bec40b5183d7c6cd9e10fea0ae47d06ba3a41d
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-27 18:17:33 +0300

    Add edit logic

--- ExamPreparation/Exam16August2021/src/views/editView.js
+++ ExamPreparation/Exam16August2021/src/views/editView.js
@@ -1,32 +1,36 @@
 import { html } from '../../node_modules/lit-html/lit-html.js'
+import * as gameService from '../services/gameService.js';
 
-const editTemplate = () => html`
+
+const editTemplate = (game) => html`
     <section id="edit-page" class="auth">
         <form id="edit">
             <div class="container">
     
                 <h1>Edit Game</h1>
                 <label for="leg-title">Legendary title:</label>
-                <input type="text" id="title" name="title" value="">
+                <input type="text" id="title" name="title" .value="${game.title}">
     
                 <label for="category">Category:</label>
-                <input type="text" id="category" name="category" value="">
+                <input type="text" id="category" name="category" .value="${game.category}">
     
                 <label for="levels">MaxLevel:</label>
-                <input type="number" id="maxLevel" name="maxLevel" min="1" value="">
+                <input type="number" id="maxLevel" name="maxLevel" min="1" .value="${game.maxLevel}">
     
                 <label for="game-img">Image:</label>
-                <input type="text" id="imageUrl" name="imageUrl" value="">
+                <input type="text" id="imageUrl" name="imageUrl" .value="${game.imageUrl}">
     
                 <label for="summary">Summary:</label>
-                <textarea name="summary" id="summary"></textarea>
+                <textarea name="summary" id="summary" .value=${game.summary}></textarea>
                 <input class="btn submit" type="submit" value="Edit Game">
-    
             </div>
         </form>
     </section>
 `;
 
-export function editPage(ctx) {
-    ctx.render(editTemplate());
+export async function editPage(ctx) {
+    const gameId = ctx.params.id;
+    const game = await gameService.getById(gameId);
+
+    ctx.render(editTemplate(game));
 }
