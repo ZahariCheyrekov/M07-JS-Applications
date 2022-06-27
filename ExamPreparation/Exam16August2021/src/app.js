e5b5565ecfa8bafc8fd438b9e73d260ff495633f
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-27 14:38:35 +0300

    Add page views

--- ExamPreparation/Exam16August2021/src/app.js
+++ ExamPreparation/Exam16August2021/src/app.js
@@ -1,9 +1,15 @@
 import page from '../node_modules/page/page.mjs';
 import { addRender } from './middlewares/render.js';
+import { catalogPage } from './views/catalogView.js';
 import { homePage } from './views/homeView.js';
+import { loginPage } from './views/loginView.js';
+import { registerPage } from './views/registerView.js';
 
 page(addRender);
 
 page('/', homePage);
+page('/catalog', catalogPage);
+page('/login', loginPage);
+page('/register', registerPage);
 
 page.start();
