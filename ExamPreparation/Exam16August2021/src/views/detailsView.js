2cfc0bcc1e07ce2f5fc3fe9febdfdeb695f8a294
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-27 17:58:39 +0300

    Add user validation logic

--- ExamPreparation/Exam16August2021/src/views/detailsView.js
+++ ExamPreparation/Exam16August2021/src/views/detailsView.js
@@ -1,4 +1,4 @@
-import { html } from '../../node_modules/lit-html/lit-html.js'
+import { html, nothing } from '../../node_modules/lit-html/lit-html.js'
 import * as gameService from '../services/gameService.js';
 
 const detailsTemplate = (game) => html`
@@ -17,36 +17,39 @@ const detailsTemplate = (game) => html`
     
             <!-- Bonus ( for Guests and Users ) -->
             <!-- <div class="details-comments">
-                <h2>Comments:</h2>
-                <ul>
-                    list all comments for current game (If any)
-                    <li class="comment">
-                        <p>Content: I rate this one quite highly.</p>
-                    </li>
-                    <li class="comment">
-                        <p>Content: The best game.</p>
-                    </li>
-                </ul>
-                Display paragraph: If there are no games in the database -->
-                <!-- <p class="no-comment">No comments.</p>
-            </div> -->
+                                    <h2>Comments:</h2>
+                                    <ul>
+                                        list all comments for current game (If any)
+                                        <li class="comment">
+                                            <p>Content: I rate this one quite highly.</p>
+                                        </li>
+                                        <li class="comment">
+                                            <p>Content: The best game.</p>
+                                        </li>
+                                    </ul>
+                                    Display paragraph: If there are no games in the database -->
+            <!-- <p class="no-comment">No comments.</p>
+                                </div> -->
     
             <!-- Edit/Delete buttons ( Only for creator of this game )  -->
+           
+            ${game.isOwner  ? html`
             <div class="buttons">
-                <a href="#" class="button">Edit</a>
-                <a href="#" class="button">Delete</a>
-            </div>
+                <a href="/edit/${game._id}" class="button">Edit</a>
+                <a href="javascript:void(0)" class="button">Delete</a>
+    	    </div>` : nothing}
+            
         </div>
     
         <!-- Bonus -->
         <!-- Add Comment ( Only for logged-in users, which is not creators of the current game ) -->
         <!-- <article class="create-comment">
-            <label>Add new comment:</label>
-            <form class="form">
-                <textarea name="comment" placeholder="Comment......"></textarea>
-                <input class="btn submit" type="submit" value="Add Comment">
-            </form>
-        </article> -->
+                                <label>Add new comment:</label>
+                                <form class="form">
+                                    <textarea name="comment" placeholder="Comment......"></textarea>
+                                    <input class="btn submit" type="submit" value="Add Comment">
+                                </form>
+                            </article> -->
     
     </section>
 `;
@@ -54,5 +57,11 @@ const detailsTemplate = (game) => html`
 export async function detailsPage(ctx) {
     const gameId = ctx.params.id;
     const game = await gameService.getById(gameId);
+
+
+    if (ctx.user) {
+        game.isOwner = ctx.user._id == game._ownerId;
+    }
+
     ctx.render(detailsTemplate(game));
 }
