35ee93c93f8faec0737f4ae2eeb6aa266782ded6
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-27 16:19:24 +0300

    Add userService login function

---
+++ ExamPreparation/Exam16August2021/src/services/userService.js
@@ -0,0 +1,14 @@
+import * as api from '../api/api.js';
+import { clearUserData, setUserData } from '../utils';
+
+const endopoints = {
+    login: '/users/login',
+    register: '/users/register',
+    logout: '/users/logout'
+};
+
+export async function login(email, password) {
+    const result = await api.post(endopoints.login, { email, password });
+    setUserData(result);
+    return result;
+}
