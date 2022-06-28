18715aafe77efcd43567cc94ac424814c96d2152
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-28 11:19:26 +0300

    Delete files

--- ExamPreparation/Exam25November2021/src/middlewares/homeMiddleware.js
+++
@@ -1,8 +0,0 @@
-import { render } from '../../node_modules/lit-html/lit-html.js';
-
-const root = document.querySelector('main');
-
-export const homeMiddleware = (ctx, next) => {
-    ctx.render = (content) => render(content, root);
-    next();
-}
--- ExamPreparation/Exam25November2021/src/middlewares/navigationMiddleware.js
+++
@@ -1,9 +0,0 @@
-import { render } from '../../node_modules/lit-html/lit-html.js';
-import { navigationView } from '../views/navigationView.js';
-
-const root = document.querySelector('#container');
-
-export const navigationMiddleware = (ctx, next) => {
-    render(navigationView(ctx), root);
-    next();
-};
