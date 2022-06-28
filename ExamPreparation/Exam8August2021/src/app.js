5c42337466b7fc377d8b233ed56282e42c78262e
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-28 12:39:15 +0300

    Add task changes

--- ExamPreparation/Exam8August2021/src/app.js
+++
@@ -1,9 +0,0 @@
-import page from '../node_modules//page/page.mjs';
-import { navigationMiddleware } from './middlewares/navigationMiddleware.js';
-import { homeView } from './views/homeView.js';
-
-page(navigationMiddleware);
-
-page('/', homeView);
-
-page.start();
--- ExamPreparation/Exam8August2021/src/services/bookService.js
+++
@@ -1,9 +0,0 @@
-import * as api from '../api/api.js';
-
-const endpoints = {
-    allGames: '/data/books?sortBy=_createdOn%20desc',
-};
-
-export async function getAllBooks() {
-    return api.get(endpoints.allGames);
-}
