54ed7022fb17e2deb2982958ac6e7f25fcfb73de
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-28 11:58:10 +0300

    Add homeView.js

--- ExamPreparation/Exam8August2021/src/app.js
+++ ExamPreparation/Exam8August2021/src/app.js
@@ -1,8 +1,9 @@
 import page from '../node_modules//page/page.mjs';
 import { navigationMiddleware } from './middlewares/navigationMiddleware.js';
+import { homeView } from './views/homeView.js';
 
 page(navigationMiddleware);
 
-// page('/',);
+page('/', homeView);
 
 page.start();
