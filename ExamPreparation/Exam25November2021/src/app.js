1a722e52e40c621ff6355e152a5c279374bb9487
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-28 11:11:26 +0300

    Add middleware logic

--- ExamPreparation/Exam25November2021/src/app.js
+++ ExamPreparation/Exam25November2021/src/app.js
@@ -1,1 +1,15 @@
-console.log('Hi!');
+import page from '../node_modules/page/page.mjs';
+
+import { homeMiddleware } from './middlewares/homeMiddleware.js';
+import { navigationMiddleware } from './middlewares/navigationMiddleware.js';
+
+import { homeView } from './views/homeView.js';
+import { loginView } from './views/loginView.js';
+
+page(navigationMiddleware);
+page(homeMiddleware);
+
+page('/', homeView);
+page('/login', loginView);
+
+page.start();
