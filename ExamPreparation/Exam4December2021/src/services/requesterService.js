13a6d097115439bcc36f3eab59a305a9485b5a81
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-29 18:22:35 +0300

    Add getAllAlbums function

--- ExamPreparation/Exam4December2021/src/services/requesterService.js
+++ ExamPreparation/Exam4December2021/src/services/requesterService.js
@@ -2,6 +2,7 @@ import * as request from './requester.js';
 import * as userService from './userService.js';
 
 const baseUrl = 'http://localhost:3030/users';
+const albumsUrl = 'http://localhost:3030/data/albums?sortBy=_createdOn%20desc&distinct=name'
 
 export const login = (email, password) =>
     request.post(`${baseUrl}/login`, { email, password })
@@ -15,3 +16,6 @@ export const logout = () =>
 export const register = (email, password) =>
     request.post(`${baseUrl}/register`, { email, password })
         .then(user => userService.saveUser(user));
+
+export const getAllAlbums = () =>
+    request.get(albumsUrl);
