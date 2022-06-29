717d5790832ffe2afc7976d0d46e7ca0b6b5bbf8
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-29 17:05:32 +0300

    Add logout function

--- ExamPreparation/Exam4December2021/src/services/requesterService.js
+++ ExamPreparation/Exam4December2021/src/services/requesterService.js
@@ -6,3 +6,8 @@ const baseUrl = 'http://localhost:3030/users';
 export const login = (email, password) =>
     request.post(`${baseUrl}/login`, { email, password })
         .then(user => userService.saveUser(user));
+
+export const logout = () =>
+    fetch(`${baseUrl}/logout`,
+        { headers: { 'X-Authorization': userService.getAccessToken() } })
+        .then(() => userService.removeUser());
