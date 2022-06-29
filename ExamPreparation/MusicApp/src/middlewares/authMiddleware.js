4896bd286558a9d3c718edc0cdd746f61ee205d5
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-29 14:18:04 +0300

    Add task files

--- ExamPreparation/MusicApp/src/middlewares/authMiddleware.js
+++
@@ -1,6 +0,0 @@
-import * as authService from '../services/authService.js';
-
-export const authMidlleware = (ctx, next) => {
-    ctx.user = authService.getUser();
-    next();
-}
--- ExamPreparation/MusicApp/src/utils/validators.js
+++
@@ -1,4 +0,0 @@
-export const albumIsInvalid = (albumData) => {
-    const requiredFields = ['name', 'imgUrl', 'price', 'releaseDate', 'artist', 'genre', 'description'];
-    return requiredFields.some(x => !albumData[x]);
-}
