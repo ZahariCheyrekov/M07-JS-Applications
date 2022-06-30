6574d2cd0da25792b25cf0979778364ebb2d98e2
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-30 12:35:39 +0300

    Add registeruser function

--- ExamPreparation/Exam8August2021/src/services/requesterService.js
+++ ExamPreparation/Exam8August2021/src/services/requesterService.js
@@ -8,6 +8,10 @@ export const loginUser = (email, password) =>
     request.post(`${baseUrl}/login`, { email, password })
         .then(user => userService.saveUser(user));
 
+export const registerUser = (email, password) =>
+    request.post(`${baseUrl}/register`, { email, password })
+        .then(user => userService.saveUser(user));
+
 export const logout = () => request.get(`${baseUrl}/logout`,
     { headers: { 'X-Authorization': userService.getAccessToken() } })
     .then(() => userService.removeUser());
