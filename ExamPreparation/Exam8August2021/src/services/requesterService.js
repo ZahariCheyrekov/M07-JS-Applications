c1d67f4e03a4e96a6661f49be2d9545a5f79cc17
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-30 17:31:35 +0300

    Add like button requests

--- ExamPreparation/Exam8August2021/src/services/requesterService.js
+++ ExamPreparation/Exam8August2021/src/services/requesterService.js
@@ -4,6 +4,7 @@ import * as userService from '../services/userService.js';
 const baseUrl = 'http://localhost:3030/users';
 const allBooksUrl = 'http://localhost:3030/data/books?sortBy=_createdOn%20desc';
 const createBookUrl = 'http://localhost:3030/data/books';
+const likeBookUrl = 'http://localhost:3030/data/likes';
 
 export const loginUser = (email, password) =>
     request.post(`${baseUrl}/login`, { email, password })
@@ -29,3 +30,11 @@ export const createBook = (data) => request.post(createBookUrl, data);
 export const editBook = (bookId, data) => request.put(`${createBookUrl}/${bookId}`, data);
 
 export const deleteBookById = (bookId) => request.del(`${createBookUrl}/${bookId}`);
+
+export const likeBookById = (bookId) => request.post(likeBookUrl,  {bookId });
+
+export const getBookLikes = (bookId) =>
+    request.get(`http://localhost:3030/data/likes?where=bookId%3D%22${bookId}%22&distinct=_ownerId&count`);
+
+export const likeForSpecificUser = (bookId, userId) =>
+    request.get(`http://localhost:3030/data/likes?where=bookId%3D%22${bookId}%22%20and%20_ownerId%3D%22${userId}%22&count`)
