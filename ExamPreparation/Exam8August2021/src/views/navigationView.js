0f81888e86a69dc88d4538ccc90f9ee656fee7fb
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-30 10:51:34 +0300

    Add navigation links

--- ExamPreparation/Exam8August2021/src/views/navigationView.js
+++ ExamPreparation/Exam8August2021/src/views/navigationView.js
@@ -1,25 +1,30 @@
 import { html } from '../../node_modules/lit-html/lit-html.js';
 
-const navigationTemplate = () => html`
+const navigationTemplate = (user) => html`
     <nav class="navbar">
         <section class="navbar-dashboard">
             <a href="#">Dashboard</a>
-            <!-- Guest users -->
-            <div id="guest">
-                <a class="button" href="#">Login</a>
-                <a class="button" href="#">Register</a>
-            </div>
-            <!-- Logged-in users -->
-            <div id="user">
-                <span>Welcome, {email}</span>
-                <a class="button" href="#">My Books</a>
-                <a class="button" href="#">Add Book</a>
-                <a class="button" href="#">Logout</a>
-            </div>
+            ${user ? userLinks() : guestLinks()}
         </section>
     </nav>
 `;
 
+const userLinks = (email) => html`
+    <div id="user">
+        <span>Welcome, ${email}</span>
+        <a class="button" href="#">My Books</a>
+        <a class="button" href="/create">Add Book</a>
+        <a class="button" href="/logout">Logout</a>
+    </div>
+`;
+
+const guestLinks = () => html`
+    <div id="guest">
+        <a class="button" href="/login">Login</a>
+        <a class="button" href="/register">Register</a>
+    </div>
+`;
+
 export const navigationView = (ctx) => {
-    return navigationTemplate(ctx);
+    return navigationTemplate(ctx.user);
 }
