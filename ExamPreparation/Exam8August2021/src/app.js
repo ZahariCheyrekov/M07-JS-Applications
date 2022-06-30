cceda427c77e6f39a4f21adf693855dc23391e65
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-30 11:15:34 +0300

    Add loginView

--- ExamPreparation/Exam8August2021/src/app.js
+++ ExamPreparation/Exam8August2021/src/app.js
@@ -4,10 +4,13 @@ import { authMiddleware } from './middlewares/authMiddleware.js';
 import { navigationMiddleware } from './middlewares/navigationMiddleaware.js';
 import { renderMiddleware } from './middlewares/renderMiddleware.js';
 
+import { loginView } from './views/loginView.js';
+
 page(authMiddleware);
 page(navigationMiddleware);
 page(renderMiddleware);
 
 page('/', () => console.log('Home'));
+page('/login', loginView);
 
 page.start();
---
+++ ExamPreparation/Exam8August2021/src/views/loginView.js
@@ -0,0 +1,38 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+
+const loginTemplate = (onSubmit) => html`
+    <section id="login-page" class="login">
+        <form @submit=${onSubmit} id="login-form" action="" method="">
+            <fieldset>
+                <legend>Login Form</legend>
+                <p class="field">
+                    <label for="email">Email</label>
+                    <span class="input">
+                        <input type="text" name="email" id="email" placeholder="Email">
+                    </span>
+                </p>
+                <p class="field">
+                    <label for="password">Password</label>
+                    <span class="input">
+                        <input type="password" name="password" id="password" placeholder="Password">
+                    </span>
+                </p>
+                <input class="button submit" type="submit" value="Login">
+            </fieldset>
+        </form>
+    </section>
+`;
+
+export const loginView = (ctx) => {
+    const onSubmit = (ev) => {
+        ev.preventDefault();
+
+        const { email, password } = Object.fromEntries(new FormData(ev.currentTarget));
+
+        
+    }
+
+    ctx.render(loginTemplate(onSubmit));
+}
+
+// if (Object.values(data).some(input => input.trim() == '')) {
