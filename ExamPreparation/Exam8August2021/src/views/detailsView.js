274d7fd50d22923ee85185dd1e902df86e937434
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-30 13:09:49 +0300

    Add detailsView

---
+++ ExamPreparation/Exam8August2021/src/views/detailsView.js
@@ -0,0 +1,33 @@
+import { html, nothing } from '../../node_modules/lit-html/lit-html.js';
+import * as requestService from '../services/requesterService.js';
+
+const detailsTemplate = (book, user) => html`
+    <section id="details-page" class="details">
+        <div class="book-information">
+            <h3>${book.title}</h3>
+            <p class="type">Type: ${book.type}</p>
+            <p class="img"><img src="${book.imageUrl}"></p>
+            <div class="actions">
+    
+                ${user ? html`
+                <a class="button" href="#">Edit</a>
+                <a class="button" href="#">Delete</a>
+                ` : nothing}
+    
+                <div class="likes">
+                    <img class="hearts" src="/images/heart.png">
+                    <span id="total-likes">Likes: 0</span>
+                </div>
+            </div>
+        </div>
+        <div class="book-description">
+            <h3>Description:</h3>
+            <p>${book.description}</p>
+        </div>
+    </section>
+`;
+
+export const detailsView = (ctx) => {
+    requestService.getBookById(ctx.params.id)
+        .then(book => ctx.render(detailsTemplate(book, ctx.user)));
+}
