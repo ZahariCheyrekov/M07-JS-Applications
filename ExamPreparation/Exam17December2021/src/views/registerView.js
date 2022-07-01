2640e9aa2a6dcd4e71dc5116d99edb0d44491d10
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-01 22:51:15 +0300

    Add registerView

---
+++ ExamPreparation/Exam17December2021/src/views/registerView.js
@@ -0,0 +1,62 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+
+import { inputValidator } from '../validators/inputValidator.js';
+import { passwordValidator } from '../validators/passwordValidator.js';
+
+import * as alertConsole from '../messages/alertMessage.js';
+import * as requestService from '../services/requesterService.js';
+
+const registerTemplate = (onSubmit) => html`
+    <section id="register">
+        <form @submit=${onSubmit} id="register-form">
+            <div class="container">
+                <h1>Register</h1>
+                <label for="username">Username</label>
+                <input id="username" type="text" placeholder="Enter Username" name="username">
+                <label for="email">Email</label>
+                <input id="email" type="text" placeholder="Enter Email" name="email">
+                <label for="password">Password</label>
+                <input id="password" type="password" placeholder="Enter Password" name="password">
+                <label for="repeatPass">Repeat Password</label>
+                <input id="repeatPass" type="password" placeholder="Repeat Password" name="repeatPass">
+                <div class="gender">
+                    <input type="radio" name="gender" id="female" value="female">
+                    <label for="female">Female</label>
+                    <input type="radio" name="gender" id="male" value="male" checked>
+                    <label for="male">Male</label>
+                </div>
+                <input type="submit" class="registerbtn button" value="Register">
+                <div class="container signin">
+                    <p>Already have an account?<a href="/login">Sign in</a>.</p>
+                </div>
+            </div>
+        </form>
+    </section>
+`;
+
+export const registerView = (ctx) => {
+    const onSubmit = (ev) => {
+        ev.preventDefault();
+
+        const data = Object.fromEntries(new FormData(ev.currentTarget));
+
+        const isInputValid = inputValidator(Object.values(data));
+
+        if (!isInputValid) {
+            alertConsole.ALL_FIELDS_ARE_REQUIRED_MESSAGE();
+            return;
+        }
+
+        const isValidPass = passwordValidator(data.password, data.repeatPass);
+
+        if (!isValidPass) {
+            alertConsole.PASSWORDS_MUST_MATCH_MESSAGE();
+            return;
+        }
+
+        requestService.register(data)
+            .then(() => ctx.page.redirect('/catalog'));
+    }
+
+    ctx.render(registerTemplate(onSubmit));
+}
