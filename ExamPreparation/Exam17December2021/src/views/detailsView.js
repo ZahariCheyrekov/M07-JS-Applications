ddd491df8055bb68feaeeb7b68846ed83e141833
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-01 23:37:20 +0300

    Add meme info

--- ExamPreparation/Exam17December2021/src/views/detailsView.js
+++ ExamPreparation/Exam17December2021/src/views/detailsView.js
@@ -4,23 +4,17 @@ import * as requestService from '../services/requesterService.js';
 
 const detailsTemplate = (meme, isOwner) => html`
     <section id="meme-details">
-        <h1>Meme Title: Bad code can present some problems
-    
-        </h1>
+        <h1>Meme Title: ${meme.title}</h1>
         <div class="meme-details">
             <div class="meme-img">
                 <img alt="meme-alt" src="${meme.imageUrl}">
             </div>
             <div class="meme-description">
                 <h2>Meme Description</h2>
-                <p>
-                    Being a programmer is a fun job. And many funny incidents occur throughout a
-                    programmer’s career.
-                    Here are a few jokes that can be relatable to you as a programmer.
-                </p>
+                <p>${meme.description}</p>
     
                 ${isOwner ? html`
-                <a class="button warning" href="#">Edit</a>
+                <a class="button warning" href="/data/memes/${meme._id}/edit">Edit</a>
                 <button class="button danger">Delete</button>` : nothing}
             </div>
         </div>
