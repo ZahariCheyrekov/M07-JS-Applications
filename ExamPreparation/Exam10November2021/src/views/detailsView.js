7b301fd9f5da2472ecbe0aba3b2a04f0b68be276
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-01 16:22:31 +0300

    Add detailsView

---
+++ ExamPreparation/Exam10November2021/src/views/detailsView.js
@@ -0,0 +1,37 @@
+import { html, nothing } from '../../node_modules/lit-html/lit-html.js';
+
+import * as requestService from '../services/requesterService.js';
+
+const detailsTemplate = (add, isOwner) => html`
+    <section id="listing-details">
+        <h1>Details</h1>
+        <div class="details-info">
+            <img src="${add.imageUrl}">
+            <hr>
+            <ul class="listing-props">
+                <li><span>Brand:</span>${add.brand}</li>
+                <li><span>Model:</span>${add.model}</li>
+                <li><span>Year:</span>${add.year}</li>
+                <li><span>Price:</span>${add.price}$</li>
+            </ul>
+    
+            <p class="description-para">${add.description}</p>
+    
+            ${isOwner ? html`
+            <div class="listings-buttons">
+                <a href="/data/cars/${add._id}/edit" class="button-list">Edit</a>
+                <a href="/data/cars/${add._id}/delete" class="button-list">Delete</a>
+            </div>` : nothing}
+        </div>
+    </section>
+`;
+
+export const detailsView = (ctx) => {
+    const addId = ctx.params.id;
+
+    requestService.getAddById(addId)
+        .then(add => {
+            const isOwner = ctx.user._id == add._ownerId;
+            ctx.render(detailsTemplate(add, isOwner))
+        });
+}
