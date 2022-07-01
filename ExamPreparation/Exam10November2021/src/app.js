e8ba7630e53fd40e667e19e42395fe93c7270c23
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-01 15:55:26 +0300

    Add app imports

--- ExamPreparation/Exam10November2021/src/app.js
+++ ExamPreparation/Exam10November2021/src/app.js
@@ -4,6 +4,9 @@ import { authMiddleware } from './middlewares/authMiddleware.js';
 import { navigationMiddleware } from './middlewares/navigationMiddleware.js';
 import { renderMiddleware } from './middlewares/renderMiddleware.js';
 
+import { logoutHandler } from './handlers/logoutHandler.js';
+
+import { homeView } from './views/homeView.js';
 import { loginView } from './views/loginView.js';
 import { registerView } from './views/registerView.js';
 
@@ -11,8 +14,9 @@ page(authMiddleware);
 page(navigationMiddleware);
 page(renderMiddleware);
 
-page('/', () => console.log('Home'));
+page('/', homeView);
 page('/login', loginView);
 page('/register', registerView);
+page('/logout', logoutHandler);
 
 page.start();
