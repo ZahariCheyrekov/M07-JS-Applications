c6eca4f00b7207d316f551f4f137723b59b0ea95
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-01 09:39:59 +0300

    Add register function

--- ExamPreparation/Exam16August2021/src/services/requesterService.js
+++ ExamPreparation/Exam16August2021/src/services/requesterService.js
@@ -6,3 +6,7 @@ const baseUrl = 'http://localhost:3030/users';
 export const login = (email, password) =>
     request.post(`${baseUrl}/login`, { email, password })
         .then(user => userService.saveUser(user));
+
+export const register = (email, password) =>
+    request.post(`${baseUrl}/register`, { email, password })
+        .then(user => userService.saveUser(user));
