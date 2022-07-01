842ac955e4cbcc21c0501f52993ba6e9b26ad676
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-01 10:51:07 +0300

    Add detailsView

--- ExamPreparation/Exam16August2021/src/views/catalogView.js
+++ ExamPreparation/Exam16August2021/src/views/catalogView.js
@@ -19,7 +19,7 @@ const gameTemplate = (game) => html`
             <img src="${game.imageUrl}">
             <h6>${game.category}</h6>
             <h2>${game.title}</h2>
-            <a href="/data/games${game._id}" class="details-button">Details</a>
+            <a href="/data/games/${game._id}" class="details-button">Details</a>
         </div>
     </div>
 `;
