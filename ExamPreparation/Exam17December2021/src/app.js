01fe6716e1b626db3ef5787f9543beb944c84d8a
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-01 23:04:26 +0300

    Add catalogView

--- ExamPreparation/Exam17December2021/src/app.js
+++ ExamPreparation/Exam17December2021/src/app.js
@@ -10,6 +10,7 @@ import { homeView } from './views/homeView.js';
 import { loginView } from './views/loginView.js';
 import { registerView } from './views/registerView.js';
 import { createView } from './views/createView.js';
+import { catalogView } from './views/catalogView.js';
 
 page(authMiddleware);
 page(navigationMiddleware);
@@ -20,5 +21,6 @@ page('/login', loginView);
 page('/register', registerView);
 page('/logout', logoutHandler);
 page('/create', createView);
+page('/catalog', catalogView);
 
 page.start();
