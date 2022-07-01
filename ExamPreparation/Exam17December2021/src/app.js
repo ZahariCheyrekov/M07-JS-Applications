d8cf5e3ca54401806bdbb33ce17531727f5cfc95
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-01 22:44:35 +0300

    Add app.jsz imports

--- ExamPreparation/Exam17December2021/src/app.js
+++ ExamPreparation/Exam17December2021/src/app.js
@@ -1,5 +1,16 @@
 import page from '../node_modules/page/page.mjs';
 
-page('/', console.log('Home'));
+import { authMiddleware } from './middlewares/authMiddleware.js';
+import { navigationMiddleware } from './middlewares/navigationMiddleware.js';
+import { renderMiddleware } from './middlewares/renderMiddleware.js';
+
+import { loginView } from './views/loginView.js';
+
+page(authMiddleware);
+page(navigationMiddleware);
+page(renderMiddleware);
+
+// page('/', console.log('Home'));
+page('/login', loginView);
 
 page.start();
