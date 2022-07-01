2da87db7ac36b41e9870d72724063d8ea3fced88
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-01 22:54:16 +0300

    Add logoutHandler

--- ExamPreparation/Exam17December2021/src/app.js
+++ ExamPreparation/Exam17December2021/src/app.js
@@ -4,6 +4,8 @@ import { authMiddleware } from './middlewares/authMiddleware.js';
 import { navigationMiddleware } from './middlewares/navigationMiddleware.js';
 import { renderMiddleware } from './middlewares/renderMiddleware.js';
 
+import { logoutHandler } from './handlers/logoutHandler.js';
+
 import { homeView } from './views/homeView.js';
 import { loginView } from './views/loginView.js';
 import { registerView } from './views/registerView.js';
@@ -15,5 +17,7 @@ page(renderMiddleware);
 page('/', homeView);
 page('/login', loginView);
 page('/register', registerView);
+page('/logout', logoutHandler);
+page('/create',);
 
 page.start();
