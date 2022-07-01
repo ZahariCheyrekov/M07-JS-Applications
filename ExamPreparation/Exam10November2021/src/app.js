f8b67e082fbf2395bfdabaf6be77152e8c8018a5
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-01 15:30:26 +0300

    Add app.js

--- ExamPreparation/Exam10November2021/src/app.js
+++ ExamPreparation/Exam10November2021/src/app.js
@@ -1,1 +1,11 @@
-console.log('Hi')
+import page from '../node_modules/page/page.mjs';
+
+import { authMiddleware } from './middlewares/authMiddleware.js';
+import { navigationMiddleware } from './middlewares/navigationMiddleware.js';
+
+page(authMiddleware);
+page(navigationMiddleware);
+
+page('/', () => console.log('Home'));
+
+page.start();
