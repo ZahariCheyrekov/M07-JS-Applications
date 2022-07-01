3a5ce27eac6e25fd61cb5e3252a18efba1ff73a7
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-01 17:45:34 +0300

    Add year search message

--- ExamPreparation/Exam10November2021/src/messages/alertMessage.js
+++ ExamPreparation/Exam10November2021/src/messages/alertMessage.js
@@ -3,3 +3,5 @@ export const ALL_FIELDS_ARE_REQUIRED_MESSAGE = () => alert('All fields are requi
 export const PASSWORDS_MUST_MATCH_MESSAGE = () => alert('Passwords must match!');
 
 export const POSITIVE_PRICE_NUMBERS_MESSAGE = () => alert('Enter positive value!');
+
+export const YEAR_SEARCH_MESSAGE = () => alert('Enter valid year!');
