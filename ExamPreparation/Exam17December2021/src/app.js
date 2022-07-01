76b0d8d2d5711513b302b4a9f134cd1c79a98f85
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-01 22:47:33 +0300

    Add homeView

--- ExamPreparation/Exam17December2021/src/app.js
+++ ExamPreparation/Exam17December2021/src/app.js
@@ -4,13 +4,14 @@ import { authMiddleware } from './middlewares/authMiddleware.js';
 import { navigationMiddleware } from './middlewares/navigationMiddleware.js';
 import { renderMiddleware } from './middlewares/renderMiddleware.js';
 
+import { homeView } from './views/homeView.js';
 import { loginView } from './views/loginView.js';
 
 page(authMiddleware);
 page(navigationMiddleware);
 page(renderMiddleware);
 
-// page('/', console.log('Home'));
+page('/', homeView);
 page('/login', loginView);
 
 page.start();
