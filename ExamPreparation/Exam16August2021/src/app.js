cde231777ac505fd684778100e6a8891120bc0c7
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-01 09:28:04 +0300

    Add authMiddleware

--- ExamPreparation/Exam16August2021/src/app.js
+++ ExamPreparation/Exam16August2021/src/app.js
@@ -1,9 +1,12 @@
 import page from '../node_modules/page/page.mjs';
 
+import { authMiddleware } from './middlewares/authMiddleware.js';
 import { navigationMiddleware } from './middlewares/navigationMiddleware.js';
 import { renderMiddleware } from './middlewares/renderMiddleware.js';
+
 import { loginView } from './views/loginView.js';
 
+page(authMiddleware);
 page(navigationMiddleware);
 page(renderMiddleware);
 
