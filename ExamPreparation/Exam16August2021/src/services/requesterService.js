cf2a47c2313e57b6496c814d58336cb21c55ce99
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-01 10:04:54 +0300

    Add urls

--- ExamPreparation/Exam16August2021/src/services/requesterService.js
+++ ExamPreparation/Exam16August2021/src/services/requesterService.js
@@ -1,7 +1,9 @@
 import * as request from '../api/requester.js';
 import * as userService from './userService.js';
 
-const baseUrl = 'http://localhost:3030/users';
+const url = 'http://localhost:3030/';
+const baseUrl = `${url}/users`;
+const allGamesUrl = `${url}/data/games?sortBy=_createdOn%20desc`;
 
 export const login = (email, password) =>
     request.post(`${baseUrl}/login`, { email, password })
