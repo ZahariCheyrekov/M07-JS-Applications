56908c5f6def518ae07f067cb10d280ca3825020
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-02 12:13:07 +0300

    Add profileView

--- ExamPreparation/Exam16December2021/src/app.js
+++ ExamPreparation/Exam16December2021/src/app.js
@@ -5,6 +5,7 @@ import { navigationMiddleware } from './middlewares/navigationMiddleware.js';
 import { renderMiddleware } from './middlewares/renderMiddleware.js';
 
 import { logoutHandler } from './handlers/logoutHandler.js';
+import { deleteHandler } from './handlers/deleteHandler.js';
 
 import { homeView } from './views/homeView.js';
 import { loginView } from './views/loginView.js';
@@ -12,6 +13,7 @@ import { registerView } from './views/registerView.js';
 import { createView } from './views/createView.js';
 import { detailsView } from './views/detailsView.js';
 import { editView } from './views/editView.js';
+import { profileView } from './views/profileView.js';
 
 page(authMiddleware);
 page(navigationMiddleware);
@@ -22,7 +24,9 @@ page('/login', loginView);
 page('/register', registerView);
 page('/logout', logoutHandler);
 page('/create', createView);
+page('/profile', profileView);
 page('/data/theaters/:id', detailsView);
 page('/data/theaters/:id/edit', editView);
+page('/data/theaters/:id/delete', deleteHandler);
 
 page.start();
