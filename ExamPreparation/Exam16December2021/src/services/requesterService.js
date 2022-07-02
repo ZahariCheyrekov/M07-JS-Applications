240e61c60f4cc0b1b3200ea8e84afbc485cad530
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-02 13:58:23 +0300

    Add service requests

--- ExamPreparation/Exam16December2021/src/services/requesterService.js
+++ ExamPreparation/Exam16December2021/src/services/requesterService.js
@@ -9,6 +9,9 @@ const theaterDetailsUrl = (theaterId) => `${theatersAllUrl}/${theaterId}`;
 const editTheaterUrl = (theaterId) => `${theatersAllUrl}/${theaterId}`;
 const deleteTheaterUrl = (theaterId) => `${theatersAllUrl}/${theaterId}`;
 const listTheatersUrl = (userId) => `${url}/data/theaters?where=_ownerId%3D%22${userId}%22&sortBy=_createdOn%20desc`;
+const likeUrl = `${url}/data/likes`;
+const likesTheaterUrl = (theaterId) => `${url}/data/likes?where=theaterId%3D%22${theaterId}%22&distinct=_ownerId&count`;
+const eventLikesUrl = (theaterId, userId) => `${url}/data/likes?where=theaterId%3D%22${theaterId}%22%20and%20_ownerId%3D%22${userId}%22&count`;
 
 export const login = (email, password) =>
     request.post(`${baseUrl}/login`, { email, password })
@@ -33,3 +36,9 @@ export const editTheater = (theaterId, data) => request.put(editTheaterUrl(theat
 export const deleteTheater = (theaterId) => request.del(deleteTheaterUrl(theaterId));
 
 export const getListTheaters = (userId) => request.get(listTheatersUrl(userId));
+
+export const addLike = (theaterId) => request.post(likeUrl, { theaterId });
+
+export const getLikesTheater = (theaterId) => request.get(likesTheaterUrl(theaterId));
+
+export const getLikesEvent = (theaterId, userId) => request.get(eventLikesUrl(theaterId, userId));
