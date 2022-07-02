230d2de762a66403a6463bab1a830b4e58ced435
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-02 11:37:56 +0300

    Add getAllTheaters function

--- ExamPreparation/Exam16December2021/src/services/requesterService.js
+++ ExamPreparation/Exam16December2021/src/services/requesterService.js
@@ -3,6 +3,7 @@ import * as userService from './userService.js';
 
 const url = 'http://localhost:3030';
 const baseUrl = `${url}/users`;
+const theatersUrl = `${url}/data/theaters?sortBy=_createdOn%20desc&distinct=title`;
 
 export const login = (email, password) =>
     request.post(`${baseUrl}/login`, { email, password })
@@ -15,3 +16,5 @@ export const register = (email, password) =>
 export const logout = () =>
     request.get(`${baseUrl}/logout`, { headers: { 'X-Authorization': userService.getAccessToken() } })
         .then(() => userService.removeUser());
+
+export const getAllTheaters = () => request.get(theatersUrl);
