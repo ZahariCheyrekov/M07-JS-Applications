2037f4189d24bbfd6efed80d8f9939b52a4982cf
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-02 09:35:47 +0300

    Import auth middleware

--- ExamPreparation/Exam10April2022/src/app.js
+++ ExamPreparation/Exam10April2022/src/app.js
@@ -1,10 +1,12 @@
 import page from '../node_modules/page/page.mjs';
 
+import { authMiddleware } from './middlewares/authMiddleware.js';
 import { renderMiddleware } from './middlewares/renderMiddleware.js';
 import { navigationMiddleware } from './middlewares/navigationMiddleware.js';
 
-page(renderMiddleware);
+page(authMiddleware);
 page(navigationMiddleware);
+page(renderMiddleware);
 
 page('/', () => console.log('Home'));
 
