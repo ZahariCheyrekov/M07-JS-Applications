279f6f01d93480ea43d665a278a4fe99434fe0b3
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-02 15:34:36 +0300

    Add request service functions

---
+++ ExamPreparation/Exam2April2022/src/services/requestService.js
@@ -0,0 +1,20 @@
+import * as request from '../api/requester.js';
+import * as userService from '../services/userService.js';
+
+
+const baseUrl = 'http://localhost:3030';
+const url = `${baseUrl}/users`;
+
+
+export const login = (email, password) =>
+    request.post(`${url}/login`, { email, password })
+        .then(user => userService.saveUser(user));
+
+export const register = (email, password) =>
+    request.post(`${url}/register`, { email, password })
+        .then(user => userService.saveUser(user));
+
+export const logout = () =>
+    request.get(`${url}/logout`)
+        .then(() => userService.removeUser());
+
