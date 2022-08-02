d17756904f16deeb15dd0e9a699ccfe2a362fd01
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-02 10:05:32 +0300

    Add request service login function

--- ExamPreparation/Exam10April2022/src/services/requestService.js
+++ ExamPreparation/Exam10April2022/src/services/requestService.js
@@ -1,3 +1,9 @@
-const baseUrl = 'http://localhost:3030';
-const url = 'http://localhost:3030/users';
+import * as request from '../api/requester.js';
+import * as userService from '../services/userService.js';
 
+const baseUrl = 'http://localhost:3030';
+const url = `${baseUrl}/users`;
+
+export const login = (email, password) =>
+    request.post(`${url}/login`, { email, password })
+        .then(user => userService.saveUser(user));
