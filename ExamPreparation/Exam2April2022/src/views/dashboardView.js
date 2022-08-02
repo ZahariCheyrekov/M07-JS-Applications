29dc78bb78f585b1e93a83e8cb6cc074e0d919e0
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-02 16:16:40 +0300

    Change pet card details link

--- ExamPreparation/Exam2April2022/src/views/dashboardView.js
+++ ExamPreparation/Exam2April2022/src/views/dashboardView.js
@@ -7,8 +7,8 @@ const dashboardTemplate = (pets) => html`
         <h2 class="dashboard-title">Services for every animal</h2>
         <div class="animals-dashboard">
             ${pets.length > 0
-        ? pets.map(pet => petCardTemplate(pet))
-        : html`
+            ? pets.map(pet => petCardTemplate(pet))
+            : html`
             <div>
                 <p class="no-pets">No pets in dashboard</p>
             </div>
@@ -24,12 +24,12 @@ const petCardTemplate = (pet) => html`
         <h2 class="name">${pet.name}</h2>
         <h3 class="breed">${pet.breed}</h3>
         <div class="action">
-            <a class="btn" href="#">Details</a>
+            <a class="btn" href="/data/pets/${pet._id}">Details</a>
         </div>
     </div>
 `;
 
 export const dashboardView = (ctx) => {
-    requestService.getPets() 
+    requestService.getPets()
         .then(pets => ctx.render(dashboardTemplate(pets)));
 }
