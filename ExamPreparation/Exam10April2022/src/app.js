78d24cb7ef69480ec864f11873e3aeb782b334db
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-02 09:32:11 +0300

    Add middlewares

--- ExamPreparation/Exam10April2022/src/app.js
+++ ExamPreparation/Exam10April2022/src/app.js
@@ -1,5 +1,11 @@
 import page from '../node_modules/page/page.mjs';
 
+import { renderMiddleware } from './middlewares/renderMiddleware.js';
+import { navigationMiddleware } from './middlewares/navigationMiddleware.js';
+
+page(renderMiddleware);
+page(navigationMiddleware);
+
 page('/', () => console.log('Home'));
 
 page.start();
