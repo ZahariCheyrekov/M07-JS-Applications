5686adc10113e1db5e3f23e382266e823b53df3c
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-02 09:54:33 +0300

    Add requester function

---
+++ ExamPreparation/Exam10April2022/src/api/requester.js
@@ -0,0 +1,31 @@
+import { getAccessToken } from '../services/userService';
+
+export const requester = (method, url, data) => {
+    const options = {
+        method,
+        headers: {}
+    };
+
+    const token = getAccessToken();
+
+    if (token) {
+        options.headers['X-Athorization'] = token;
+    }
+
+    if (data) {
+        options.headers['Content-Type'] = 'application/json';
+
+        if (method !== 'GET') {
+            options.body = JSON.stringify(data);
+        }
+    }
+
+    return fetch(url, options)
+        .then(res => res.json());
+}
+
+export const get = requester.bind(null, 'GET');
+export const post = requester.bind(null, 'POST');
+export const put = requester.bind(null, 'PUT');
+export const patch = requester.bind(null, 'PATCH');
+export const del = requester.bind(null, 'DELETE');
