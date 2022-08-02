18d69115700e55f734165eaa91650f2d8cd2bc40
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-02 12:13:27 +0300

    Add get user posts request function

--- ExamPreparation/Exam10April2022/src/services/requestService.js
+++ ExamPreparation/Exam10April2022/src/services/requestService.js
@@ -1,8 +1,10 @@
 import * as request from '../api/requester.js';
 import * as userService from '../services/userService.js';
 
+
 const baseUrl = 'http://localhost:3030';
 const url = `${baseUrl}/users`;
+
 
 export const login = (email, password) =>
     request.post(`${url}/login`, { email, password })
@@ -16,12 +18,20 @@ export const logout = () =>
     request.get(`${url}/logout`)
         .then(() => userService.removeUser());
 
-export const editPostById = (postId, data) => request.put(`${baseUrl}/data/posts/${postId}`, data);
+export const editPostById = (postId, data) =>
+    request.put(`${baseUrl}/data/posts/${postId}`, data);
 
-export const getPostById = (postId) => request.get(`${baseUrl}/data/posts/${postId}`);
+export const getPostById = (postId) =>
+    request.get(`${baseUrl}/data/posts/${postId}`);
 
-export const getPosts = () => request.get(`${baseUrl}/data/posts?sortBy=_createdOn%20desc`);
+export const getPosts = () =>
+    request.get(`${baseUrl}/data/posts?sortBy=_createdOn%20desc`);
 
-export const create = (data) => request.post(`${baseUrl}/data/posts`, data);
+export const getUserPosts = (userId) =>
+    request.get(`${baseUrl}/data/posts?where=_ownerId%3D%22${userId}%22&sortBy=_createdOn%20desc`);
 
-export const deletePost = (postId) => request.del(`${baseUrl}/data/posts/${postId}`);
+export const create = (data) =>
+    request.post(`${baseUrl}/data/posts`, data);
+
+export const deletePost = (postId) =>
+    request.del(`${baseUrl}/data/posts/${postId}`);
