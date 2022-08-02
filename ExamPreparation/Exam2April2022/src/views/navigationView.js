dd4ec70fda873a8383fb524e00984017d42d9428
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-02 15:30:30 +0300

    Remove header navigation

---
+++ ExamPreparation/Exam2April2022/src/views/navigationView.js
@@ -0,0 +1,32 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+
+const navigationTemplate = (user) => html`
+    <nav>
+        <section class="logo">
+            <img src="./images/logo.png" alt="logo">
+        </section>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Dashboard</a></li>
+    
+            ${user
+                ? userNavigation()
+                : guestNavigation()
+            }
+        </ul>
+    </nav>
+`;
+
+const userNavigation = () => html`
+    <li><a href="/create">Create Postcard</a></li>
+    <li><a href="/logout">Logout</a></li>
+`;
+
+const guestNavigation = () => html`
+    <li><a href="/login">Login</a></li>
+    <li><a href="/register">Register</a></li>
+`;
+
+export const navigationView = (ctx) => {
+    return navigationTemplate(ctx.user);
+}
