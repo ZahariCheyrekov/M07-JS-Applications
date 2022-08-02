ef3d43daf5987456125dad8f35c3033b4c207df9
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-02 09:48:52 +0300

    Add login view

---
+++ ExamPreparation/Exam10April2022/src/views/loginView.js
@@ -0,0 +1,41 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+import { inputValidator } from '../validators/inputValidator.js';
+
+const loginTemplate = (onSubmit) => html`
+    <section id="login-page" class="auth">
+        <form @submit=${onSubmit} id="login">
+            <h1 class="title">Login</h1>
+    
+            <article class="input-group">
+                <label for="login-email">Email: </label>
+                <input type="email" id="login-email" name="email">
+            </article>
+    
+            <article class="input-group">
+                <label for="password">Password: </label>
+                <input type="password" id="password" name="password">
+            </article>
+    
+            <input type="submit" class="btn submit-btn" value="Log In">
+        </form>
+    </section>
+`;
+
+export const loginView = (ctx) => {
+    const onSubmit = (ev) => {
+        ev.preventDefault();
+
+        const data = Object.fromEntries(new FormData(ev.currentTarget));
+
+        const isValid = inputValidator(data);
+
+        if (!isValid) {
+            return;
+        }
+
+        requestService.login(data.email, data.password)
+            .then(() => ctx.page.redirect('/'));
+    }
+
+    ctx.render(loginTemplate(onSubmit));
+}
