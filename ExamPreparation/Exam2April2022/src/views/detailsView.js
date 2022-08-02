6f8c68fe9fc4a6708d90bfb1bad7d6ee6670f5f6
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-02 16:37:53 +0300

    Edit details view

--- ExamPreparation/Exam2April2022/src/views/detailsView.js
+++ ExamPreparation/Exam2April2022/src/views/detailsView.js
@@ -3,7 +3,7 @@ import { html, nothing } from '../../node_modules/lit-html/lit-html.js';
 import * as userService from '../services/userService.js';
 import * as requestService from '../services/requestService.js';
 
-const detailsTemplate = (pet, user) => html`
+const detailsTemplate = (pet, isOwner, onDonation, donations, hasDonationBtn) => html`
     <section id="detailsPage">
         <div class="details">
             <div class="animalPic">
@@ -15,22 +15,22 @@ const detailsTemplate = (pet, user) => html`
                     <h3>Breed: ${pet.breed}</h3>
                     <h4>Age: ${pet.age}</h4>
                     <h4>Weight: ${pet.weight}</h4>
-                    <h4 class="donation">Donation: 0$</h4>
-                    <!-- TODO: Check donation -->
+                    <h4 class="donation">Donation: ${donations}$</h4>
                 </div>
     
-                ${user
-                ? html`
                 <div class="actionBtn">
-                    <!-- Only for registered user and creator of the pets-->
-                    <a href="/data/pets/${pet._id}/edit" class="edit">Edit</a>
-                    <a href="/data/pets/${pet._id}/delete" class="remove">Delete</a>
-                    <!--(Bonus Part) Only for no creator and user-->
-                    <a href="#" class="donate">Donate</a>
+                    ${isOwner 
+                    ? html`
+                     <a href="/data/pets/${pet._id}/edit" class="edit">Edit</a>
+                    <a href="/data/pets/${pet._id}/delete" class="remove">Delete</a>`
+                    : nothing
+                    }
+                    
+                    ${hasDonationBtn
+                    ? html`<a href="#" class="donate" @click=${onDonation}>Donate</a>`
+                    : nothing
+                    }
                 </div>
-                `
-                : nothing
-                }
             </div>
         </div>
     </section>
@@ -38,8 +38,22 @@ const detailsTemplate = (pet, user) => html`
 
 export const detailsView = async (ctx) => {
     const user = userService.getUser();
+    const userId = user._id;
     const petId = ctx.params.id;
 
-    requestService.getPetById(petId)
-        .then(pet => ctx.render(detailsTemplate(pet, user)));
+    const onDonation = () => {
+        requestService.addDonation(petId)
+            .then(() => ctx.page.redirect(`/data/pets/${petId}`));
+    }
+
+    const [pet, donations, hasDonation] = await Promise.all([
+        requestService.getPetById(petId),
+        requestService.getPetDonations(petId),
+        user ? requestService.getDonationForPet(petId, userId) : 0
+    ]);
+
+    const isOwner = user && userId == pet._ownerId;
+    const hasDonationBtn = user && !isOwner && hasDonation === 0;
+
+    ctx.render(detailsTemplate(pet, isOwner, onDonation, donations, hasDonationBtn));
 }
