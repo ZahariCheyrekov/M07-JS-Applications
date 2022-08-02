c416d58c315fa7cf4ae856650f0c4382666a8cc6
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-02 09:32:44 +0300

    Add navigation view

---
+++ ExamPreparation/Exam10April2022/src/views/navigationView.js
@@ -0,0 +1,29 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+
+const navigationTemplate = (user) => html`
+    <h1><a href="/">Orphelp</a></h1>
+    
+    <nav>
+        <a href="/">Dashboard</a>
+        ${user ? userNavigation() : guestNavigation()}
+    </nav>
+`;
+
+const userNavigation = () => html`
+    <div id="user">
+        <a href="#">My Posts</a>
+        <a href="/create">Create Post</a>
+        <a href="/logout">Logout</a>
+    </div>
+`;
+
+const guestNavigation = () => html`
+    <div id="guest">
+        <a href="/login">Login</a>
+        <a href="/register">Register</a>
+    </div>
+`;
+
+export const navigationView = (ctx) => {
+    return navigationTemplate(ctx.user);
+}
