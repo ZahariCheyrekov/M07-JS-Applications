6ccea389335bb0f1a6fde0391a28c93a71fee9f9
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-02 09:32:20 +0300

    Import navigation view

--- ExamPreparation/Exam10April2022/src/middlewares/navigationMiddleware.js
+++ ExamPreparation/Exam10April2022/src/middlewares/navigationMiddleware.js
@@ -1,4 +1,5 @@
 import { render } from '../../node_modules/lit-html/lit-html.js';
+import { navigationView } from '../views/navigationView.js';
 
 const headerRoot = document.getElementById('header-navigation');
 
