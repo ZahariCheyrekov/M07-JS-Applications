8ba588d610d340c666cbbd397f7617472ad0a7ec
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-03 17:08:31 +0300

    Change game template link

--- ExamPreparation/Exam16August2021/src/views/catalogView.js
+++ ExamPreparation/Exam16August2021/src/views/catalogView.js
@@ -8,7 +8,7 @@ const catalogTemplate = (games) => html`
         ${games.length > 0
         ? games.map(game => gameTemplate(game))
         : html`<h3 class="no-articles">No articles yet</h3>`
-        }
+    }
     </section>
 `;
 
@@ -18,7 +18,7 @@ const gameTemplate = (game) => html`
             <img src="${game.imageUrl}">
             <h6>${game.category}</h6>
             <h2>${game.title}</h2>
-            <a href="#" class="details-button">Details</a>
+            <a href="/data/games/${game._id}" class="details-button">Details</a>
         </div>
     </div>
 `;
