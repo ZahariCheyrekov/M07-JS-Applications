54e145d9d49927c417f42ef7c6d4bc2087420456
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-03 16:03:54 +0300

    Remove task files

--- ExamPreparation/Exam16August2021/src/handlers/logoutHandler.js
+++
@@ -1,6 +0,0 @@
-import * as requestService from '../services/requesterService.js';
-
-export const logoutHandler = (ctx) => {
-    requestService.logout()
-        .then(() => ctx.page.redirect('/'));
-}
--- ExamPreparation/Exam16August2021/src/middlewares/authMiddleware.js
+++
@@ -1,6 +0,0 @@
-import * as userService from '../services/userService.js';
-
-export const authMiddleware = (ctx, next) => {
-    ctx.user = userService.getUser();
-    next();
-}
