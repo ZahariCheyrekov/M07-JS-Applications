585560eb6556202e77c6df1c4ee613b2302da5a3
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-03 16:21:56 +0300

    Add application middlewares

--- ExamPreparation/Exam16August2021/src/app.js
+++ ExamPreparation/Exam16August2021/src/app.js
@@ -1,5 +1,13 @@
 import page from '../node_modules/page/page.mjs';
 
+import { authMiddleware } from './middlewares/authMiddleware.js';
+import { navigationMiddleware } from './middlewares/navigationMiddleware.js';
+import { renderMiddleware } from './middlewares/renderMiddleware.js';
+
+page(authMiddleware);
+page(navigationMiddleware);
+page(renderMiddleware);
+
 page('/', () => console.log('Home!'));
 
 page.start();
