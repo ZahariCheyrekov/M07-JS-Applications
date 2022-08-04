2be78021bfcdcbe7fe6b7f65d8cd2f3ac57a156b
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-04 14:03:21 +0300

    Import login view

--- ExamPreparation/Exam17December2021/src/app.js
+++ ExamPreparation/Exam17December2021/src/app.js
@@ -7,11 +7,13 @@ import { renderMiddleware } from './middlewares/renderMiddleware.js';
 import { homeHandler } from './handlers/homeHandler.js';
 
 import { homeView } from './views/homeView.js';
+import { loginView } from './views/loginView.js';
 
 page(authMiddleware);
 page(navigationMiddleware);
 page(renderMiddleware);
 
 page('/', homeHandler, homeView);
+page('/login', loginView);
 
 page.start();
