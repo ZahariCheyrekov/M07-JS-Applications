a7435217c7cb504a2d082c4a3f38688be312c0c0
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-04 14:12:27 +0300

    Add create view

---
+++ ExamPreparation/Exam17December2021/src/views/createView.js
@@ -0,0 +1,42 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+
+import * as requestService from '../services/requestService.js';
+import { inputValidator } from '../validators/inputValidator.js';
+import { ALL_FIELDS_ARE_REQUIRED_MESSAGE } from '../messages/alertMessages.js';
+
+const createTemplate = (onSubmit) => html`
+    <section id="create-meme">
+        <form @submit=${onSubmit} id="create-form">
+            <div class="container">
+                <h1>Create Meme</h1>
+                <label for="title">Title</label>
+                <input id="title" type="text" placeholder="Enter Title" name="title">
+                <label for="description">Description</label>
+                <textarea id="description" placeholder="Enter Description" name="description"></textarea>
+                <label for="imageUrl">Meme Image</label>
+                <input id="imageUrl" type="text" placeholder="Enter meme ImageUrl" name="imageUrl">
+                <input type="submit" class="registerbtn button" value="Create Meme">
+            </div>
+        </form>
+    </section>
+`;
+
+export const createView = (ctx) => {
+    const onSubmit = (ev) => {
+        ev.preventDefault();
+
+        const data = Object.fromEntries(new FormData(ev.currentTarget));
+
+        const isValid = inputValidator(data);
+
+        if (!isValid) {
+            alert(ALL_FIELDS_ARE_REQUIRED_MESSAGE);
+            return;
+        }
+
+        requestService.createMeme(data)
+            .then(() => ctx.page.redirect('/all-memes'));
+    }
+
+    ctx.render(createTemplate(onSubmit));
+}
