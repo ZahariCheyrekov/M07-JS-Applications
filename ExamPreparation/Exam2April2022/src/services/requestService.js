de789045a5c6adca6605915a6b6c8ea892a49a27
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-05 11:21:41 +0300

    Add request serivce get all pets function

--- ExamPreparation/Exam2April2022/src/services/requestService.js
+++ ExamPreparation/Exam2April2022/src/services/requestService.js
@@ -17,3 +17,6 @@ export const register = (email, password) =>
 export const logout = () =>
     request.get(`${url}/logout`)
         .then(() => userService.removeUser());
+
+export const getAllPets = () =>
+    request.get(`${baseUrl}/data/pets?sortBy=_createdOn%20desc&distinct=name`);
