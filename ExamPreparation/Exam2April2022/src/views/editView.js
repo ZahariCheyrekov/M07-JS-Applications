ecfe1571562d892e8137aecd8db39bc6c7cf4ae1
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-05 11:40:40 +0300

    Create edit view

---
+++ ExamPreparation/Exam2April2022/src/views/editView.js
@@ -0,0 +1,59 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+
+import * as requestService from '../services/requestService.js';
+import { inputValidator } from '../validators/inputValidator.js';
+import { ALL_FIELDS_ARE_REQUIRED_MESSAGE } from '../messages/alertMessages.js';
+
+const editTemplate = (onSubmit, pet) => html`
+    <section id="editPage">
+        <form @submit=${onSubmit} class="editForm">
+            <img src="./images/editpage-dog.jpg">
+            <div>
+                <h2>Edit PetPal</h2>
+                <div class="name">
+                    <label for="name">Name:</label>
+                    <input name="name" id="name" type="text" value="${pet.name}">
+                </div>
+                <div class="breed">
+                    <label for="breed">Breed:</label>
+                    <input name="breed" id="breed" type="text" value="${pet.breed}">
+                </div>
+                <div class="Age">
+                    <label for="age">Age:</label>
+                    <input name="age" id="age" type="text" value="${pet.age}">
+                </div>
+                <div class="weight">
+                    <label for="weight">Weight:</label>
+                    <input name="weight" id="weight" type="text" value="${pet.wight}">
+                </div>
+                <div class="image">
+                    <label for="image">Image:</label>
+                    <input name="image" id="image" type="text" value="${pet.image}">
+                </div>
+                <button class="btn" type="submit">Edit Pet</button>
+            </div>
+        </form>
+    </section>
+`;
+
+export const editView = (ctx) => {
+    const petId = ctx.params.id;
+
+    const onSubmit = (ev) => {
+        ev.preventDefault();
+
+        const data = Object.fromEntries(new FormData(ev.currentTarget));
+
+        const isValid = inputValidator(data);
+        if (!isValid) {
+            alert(ALL_FIELDS_ARE_REQUIRED_MESSAGE);
+            return;
+        }
+
+        requestService.editPet(petId, data)
+            .then(() => ctx.page.redirect(`/data/pets/${petId}`));
+    }
+
+    requestService.getPetById(petId)
+        .then(pet => ctx.render(editTemplate(onSubmit, pet)));
+}
