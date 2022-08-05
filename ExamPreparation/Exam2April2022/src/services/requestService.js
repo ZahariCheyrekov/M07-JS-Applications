c17c8dbb769527dc0a2157e13f46f74e3cada17f
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-05 11:45:18 +0300

    Add delete pet request service function

--- ExamPreparation/Exam2April2022/src/services/requestService.js
+++ ExamPreparation/Exam2April2022/src/services/requestService.js
@@ -31,4 +31,4 @@ export const editPet = (petId, data) =>
     request.put(`${baseUrl}/data/pets/${petId}`, data);
 
 export const deletePet = (petId) =>
-    request.del(`${baseUrl}/data/pets/${petId}`);
+    request.del(`${baseUrl}/data/pets/${petId}`); 
