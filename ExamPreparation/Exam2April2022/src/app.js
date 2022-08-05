c1bdc8e5af6179577d1371bcd3e40ac5dbde72fb
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-05 10:57:47 +0300

    Import render middleware

--- ExamPreparation/Exam2April2022/src/app.js
+++ ExamPreparation/Exam2April2022/src/app.js
@@ -1,8 +1,11 @@
 import page from '../node_modules/page/page.mjs';
+
 import { authMiddleware } from './middlewares/authMiddleware.js';
 import { navigationMiddleware } from './middlewares/navigationMiddleware.js';
+import { renderMiddleware } from './middlewares/renderMiddleware.js';
 
 page(authMiddleware);
 page(navigationMiddleware);
+page(renderMiddleware);
 
 page.start();
