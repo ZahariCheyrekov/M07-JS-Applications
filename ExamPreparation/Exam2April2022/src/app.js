0b5f171012af060c80b592c32d7dbcbb4df5c4d0
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-05 10:57:30 +0300

    Import navigation middleware

--- ExamPreparation/Exam2April2022/src/app.js
+++ ExamPreparation/Exam2April2022/src/app.js
@@ -1,6 +1,8 @@
 import page from '../node_modules/page/page.mjs';
 import { authMiddleware } from './middlewares/authMiddleware.js';
+import { navigationMiddleware } from './middlewares/navigationMiddleware.js';
 
 page(authMiddleware);
+page(navigationMiddleware);
 
 page.start();
