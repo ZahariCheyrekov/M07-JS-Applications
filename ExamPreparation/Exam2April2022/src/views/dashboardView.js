b9b27861f7644821cec1359cd15c3cad92a72913
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-05 11:21:27 +0300

    Create dashboard view

---
+++ ExamPreparation/Exam2April2022/src/views/dashboardView.js
@@ -0,0 +1,37 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+
+import * as requestService from '../services/requestService.js';
+
+const dashboardTemplate = (pets) => html`
+    <section id="dashboard">
+        <h2 class="dashboard-title">Services for every animal</h2>
+        <div class="animals-dashboard">
+    
+            ${pets.length > 0
+            ? pets.map(pet => petTemplate(pet))        
+            : html`
+                <div>
+                    <p class="no-pets">No pets in dashboard</p>
+                </div>`
+            }
+        </div>
+    </section>
+`;
+
+const petTemplate = (pet) => html`
+    <div class="animals-board">
+        <article class="service-img">
+            <img class="animal-image-cover" src="${pet.image}">
+        </article>
+        <h2 class="name">${pet.naem}</h2>
+        <h3 class="breed">${pet.breed}l</h3>
+        <div class="action">
+            <a class="btn" href="#">Details</a>
+        </div>
+    </div>
+`;
+
+export const dashboardView = (ctx) => {
+    requestService.getAllPets()
+        .then(pets => ctx.render(dashboardTemplate(pets)));
+}
