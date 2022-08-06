d8181d2a0e64a691e9c665bd19e3751bf18f055e
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-06 14:02:53 +0300

    Add user logout function

---
+++ ExamPreparation/Exam6August2022/src/handlers/logoutHandler.js
@@ -0,0 +1,6 @@
+import * as requestService from '../services/requestService.js';
+
+export const logoutHandler = (ctx) => {
+    requestService.logout()
+        .then(() => ctx.page.redirect('/dashboard'));
+}
